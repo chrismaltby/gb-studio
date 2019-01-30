ebc0db8201b5253f0755b3739fbc25cfa4a50a39
Chris Maltby <chris.maltby@gmail.com>
2019-01-30 19:10:43 +0000

    Reducer tests

--- test/reducers/toolsReducer.test.js
+++ test/reducers/toolsReducer.test.js
@@ -23,3 +23,22 @@ test("Should switch back to select when clicking trigger", () => {
   const newState = toolsReducer(state, action);
   expect(newState.selected).toBe("select");
 });
+
+test("Unknown action should return input state", () => {
+  const state = {
+    selected: "map"
+  };
+  const action = {
+    type: "UNKNOWN_ACTION"
+  };
+  const newState = toolsReducer(state, action);
+  expect(newState.selected).toBe("map");
+});
+
+test("Default state should use select tool", () => {
+  const action = {
+    type: "UNKNOWN_ACTION"
+  };
+  const newState = toolsReducer(undefined, action);
+  expect(newState.selected).toBe("select");
+});
