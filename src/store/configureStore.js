c7dfa79c41d3692ff906d79902857e7575d36407
Chris Maltby <chris.maltby@gmail.com>
2019-02-19 14:35:40 +0000

    Fixed redux on production build

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,19 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "../reducers/rootReducer";
-import { composeWithDevTools } from 'redux-devtools-extension';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function configureStore() {
-  // return createStore(
-  //   rootReducer,
-  //   window.__REDUX_DEVTOOLS_EXTENSION__ &&
-  //     window.__REDUX_DEVTOOLS_EXTENSION__(),
-  //   applyMiddleware(thunk)
-  // );
-
   return createStore(
     rootReducer,
-    composeWithDevTools(
+    composeEnhancers(
       applyMiddleware(thunk)
     )
   );
