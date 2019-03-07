23e4d1dd32c0924b5ff206c4cf8bf9b58982b103
Chris Maltby <chris.maltby@gmail.com>
2019-03-07 23:19:51 +0000

    Set Toolbar left offset only on mac

--- src/lib/electron/handleFullScreen.js
+++ src/lib/electron/handleFullScreen.js
@@ -1,11 +1,11 @@
 import { ipcRenderer } from "electron";
 
 ipcRenderer.on("enter-full-screen", () => {
-  document.body.className = "full-screen";
+  document.body.className = `Platform__${process.platform} full-screen`;
 });
 
 ipcRenderer.on("leave-full-screen", () => {
-  document.body.className = "";
+  document.body.className = `Platform__${process.platform}`;
 });
 
 ipcRenderer.send("check-full-screen", {});
