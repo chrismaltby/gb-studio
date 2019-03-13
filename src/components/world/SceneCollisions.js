d8f97a7b6c306eeb2635a92d44461b9aa52b9def
Chris Maltby <chris.maltby@gmail.com>
2019-03-13 22:26:26 +0000

    Using accent colour throughout app

--- src/components/world/SceneCollisions.js
+++ src/components/world/SceneCollisions.js
@@ -21,7 +21,7 @@ class SceneCollisions extends Component {
     if (this.canvas.current) {
       this.canvas.current.width = this.canvas.current.width; // Clear canvas
       const ctx = this.canvas.current.getContext("2d");
-      ctx.fillStyle = "rgba(255,0,0,0.5)";
+      ctx.fillStyle = "rgba(255,0,0,0.6)";
       for (let yi = 0; yi < height; yi++) {
         for (let xi = 0; xi < width; xi++) {
           const collisionIndex = width * yi + xi;
