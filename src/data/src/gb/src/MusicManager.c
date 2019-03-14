201e73d27e8087e263eb7569af8572442a390020
Chris Maltby <chris.maltby@gmail.com>
2019-03-14 23:27:50 +0000

    Reading music bank correctly (was also banked)

--- src/data/src/gb/src/MusicManager.c
+++ src/data/src/gb/src/MusicManager.c
@@ -2,12 +2,16 @@
 #include "BankManager.h"
 #include "gbt_player.h"
 #include "data_ptrs.h"
+#include "BankData.h"
 
 void MusicPlay(UBYTE index, UBYTE loop, UBYTE return_bank)
 {
+  UBYTE music_bank = ReadBankedUBYTE(16, &music_banks[index]);
+
   PUSH_BANK(return_bank);
-  gbt_play(music_tracks[index], music_banks[index], 7);
+  gbt_play(music_tracks[index], music_bank, 7);
   gbt_loop(loop);
+
   POP_BANK;
 }
 
