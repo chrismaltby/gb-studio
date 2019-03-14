15f759165ee315d3d8580e6cc4c55aadd1631c94
Chris Maltby <chris@feed.xyz>
2019-03-14 15:40:55 +0000

    Event position marker helper

--- src/components/world/EventHelper.js
+++ src/components/world/EventHelper.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { EVENT_CAMERA_MOVE_TO } from "../../lib/compiler/eventTypes";
+import {
+  EVENT_CAMERA_MOVE_TO,
+  EVENT_ACTOR_MOVE_TO,
+  EVENT_ACTOR_SET_POSITION
+} from "../../lib/compiler/eventTypes";
 
 const TILE_SIZE = 8;
 
@@ -13,6 +17,15 @@ class EventHelper extends Component {
         {event.command === EVENT_CAMERA_MOVE_TO ? (
           <div
             className="EventHelper__CameraPos"
+            style={{
+              left: (event.args.x || 0) * TILE_SIZE,
+              top: (event.args.y || 0) * TILE_SIZE
+            }}
+          />
+        ) : event.command === EVENT_ACTOR_MOVE_TO ||
+          event.command === EVENT_ACTOR_SET_POSITION ? (
+          <div
+            className="EventHelper__PosMarker"
             style={{
               left: (event.args.x || 0) * TILE_SIZE,
               top: (event.args.y || 0) * TILE_SIZE
