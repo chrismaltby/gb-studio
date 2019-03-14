f6d100211d215249be06538fab5ff04650c0075c
Chris Maltby <chris.maltby@gmail.com>
2019-03-14 19:40:25 +0000

    Pause music on window blur

--- src/middleware/music.js
+++ src/middleware/music.js
@@ -13,10 +13,13 @@ function initMusic() {
   modPlayer = new ScripTracker();
   modPlayer.on(ScripTracker.Events.playerReady, onSongLoaded);
   window.removeEventListener("click", initMusic);
+  window.removeEventListener("keydown", initMusic);
 }
 
 // Initialise audio on first click
 window.addEventListener("click", initMusic);
+window.addEventListener("keydown", initMusic);
+window.addEventListener("blur", pause);
 
 function onSongLoaded(player) {
   console.log("ONLOADED");
