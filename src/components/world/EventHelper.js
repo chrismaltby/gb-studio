fbbbc0d6df365362ced278d7eb34fba55c3e8085
Chris Maltby <chris.maltby@gmail.com>
2019-03-17 16:41:23 +0000

    Overlay helper added to Overlay Set Position

--- src/components/world/EventHelper.js
+++ src/components/world/EventHelper.js
@@ -5,7 +5,8 @@ import {
   EVENT_ACTOR_MOVE_TO,
   EVENT_ACTOR_SET_POSITION,
   EVENT_OVERLAY_SHOW,
-  EVENT_OVERLAY_MOVE_TO
+  EVENT_OVERLAY_MOVE_TO,
+  EVENT_OVERLAY_SET_POSITION
 } from "../../lib/compiler/eventTypes";
 
 const TILE_SIZE = 8;
@@ -34,7 +35,8 @@ class EventHelper extends Component {
             }}
           />
         ) : event.command === EVENT_OVERLAY_SHOW ||
-          event.command === EVENT_OVERLAY_MOVE_TO ? (
+          event.command === EVENT_OVERLAY_MOVE_TO ||
+          event.command === EVENT_OVERLAY_SET_POSITION ? (
           <div className="EventHelper__OverlayPos">
             <div
               className="EventHelper__OverlayPos__Overlay"
