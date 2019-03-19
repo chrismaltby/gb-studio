5114b2931fd89751cece9dbc85a88a8ca9ea0d59
Chris Maltby <chris@feed.xyz>
2019-03-19 11:48:42 +0000

    If same music track played twice doesn't restart it

--- data/src/gb/src/MusicManager.c
+++ data/src/gb/src/MusicManager.c
@@ -4,20 +4,31 @@
 #include "data_ptrs.h"
 #include "BankData.h"
 
+#define MAX_MUSIC 255
+
+UBYTE current_index = MAX_MUSIC;
+
 void MusicPlay(UBYTE index, UBYTE loop, UBYTE return_bank)
 {
-  UBYTE music_bank = ReadBankedUBYTE(16, &music_banks[index]);
+  UBYTE music_bank;
 
-  PUSH_BANK(return_bank);
-  gbt_play(music_tracks[index], music_bank, 7);
-  gbt_loop(loop);
+  if (index != current_index)
+  {
+    current_index = index;
+    music_bank = ReadBankedUBYTE(16, &music_banks[index]);
 
-  POP_BANK;
+    PUSH_BANK(return_bank);
+    gbt_play(music_tracks[index], music_bank, 7);
+    gbt_loop(loop);
+
+    POP_BANK;
+  }
 }
 
 void MusicStop(UBYTE return_bank)
 {
   PUSH_BANK(return_bank);
   gbt_stop();
+  current_index = MAX_MUSIC;
   POP_BANK;
 }
