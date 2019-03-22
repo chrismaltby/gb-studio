f6d0eab3b3267ba4a899a6c920199a01a4d0d0b0
chrismaltby <chris.maltby@gmail.com>
2019-03-22 12:18:29 +0000

    Fixed music compile on Mac

--- appData/src/gb/src/MusicManager.c
+++ appData/src/gb/src/MusicManager.c
@@ -11,16 +11,14 @@ UBYTE current_index = MAX_MUSIC;
 void MusicPlay(UBYTE index, UBYTE loop, UBYTE return_bank)
 {
   UBYTE music_bank;
-  UWORD music_track;
 
   if (index != current_index)
   {
     current_index = index;
     music_bank = ReadBankedUBYTE(16, &music_banks[index]);
-    music_track = music_tracks[index];
 
     PUSH_BANK(return_bank);
-    gbt_play(music_track, music_bank, 7);
+    gbt_play(music_tracks[index], music_bank, 7);
     gbt_loop(loop);
 
     POP_BANK;
