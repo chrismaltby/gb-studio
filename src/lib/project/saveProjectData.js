8e4d90dcaa33e5fde1428f23d0752e3b77f78efc
Chris Maltby <chris.maltby@gmail.com>
2019-03-23 18:40:30 +0000

    Switched to writeFileSync when saving project to remove edge case where file might write multiple times

--- src/lib/project/saveProjectData.js
+++ src/lib/project/saveProjectData.js
@@ -2,9 +2,7 @@ import fs from "fs-extra";
 import path from "path";
 
 const saveProjectData = async (projectPath, project) => {
-  console.log("SAVING PROJECT TO ", projectPath);
-  await fs.writeFile(projectPath, JSON.stringify(project, null, 4));
-  console.log("WROTE TO ", projectPath);
+  fs.writeFileSync(projectPath, JSON.stringify(project, null, 4));
 };
 
 export default saveProjectData;
