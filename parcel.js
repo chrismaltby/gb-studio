73c3619d92c46b4cb529dae3a93133880b0ec403
Chris Maltby <chris.maltby@gmail.com>
2019-03-23 00:16:20 +0000

    Using parcel build again

--- parcel.js
+++ parcel.js
@@ -1,7 +1,8 @@
 const Bundler = require("parcel-bundler");
+const fs = require("fs-extra");
 const Path = require("path");
 
-console.log("ENV=" + process.env.NODE_ENV);
+const isDevMode = process.env.NODE_ENV !== "production";
 
 // Single entrypoint file location:
 const entryFiles = [Path.join(__dirname, "./src/index.js")];
@@ -14,15 +15,15 @@ const options = {
   outDir: "./dist", // The out directory to put the build files in, defaults to dist
   // outFile: 'index.html', // The name of the outputFile
   publicUrl: "./", // The url to serve on, defaults to '/'
-  watch: true, // Whether to watch the files and rebuild them on change, defaults to process.env.NODE_ENV !== 'production'
-  cache: true, // Enabled or disables caching, defaults to true
+  watch: isDevMode, // Whether to watch the files and rebuild them on change, defaults to process.env.NODE_ENV !== 'production'
+  cache: isDevMode, // Enabled or disables caching, defaults to true
   cacheDir: ".parcelCache", // The directory cache gets put in, defaults to .cache
   contentHash: false, // Disable content hash from being included on the filename
   target: "electron", // Browser/node/electron, defaults to browser
   // logLevel: 3, // 5 = save everything to a file, 4 = like 3, but with timestamps and additionally log http requests to dev server, 3 = log info, warnings & errors, 2 = log warnings & errors, 1 = log errors
-  hmr: true, // Enable or disable HMR while watching
+  hmr: isDevMode, // Enable or disable HMR while watching
   hmrPort: 0, // The port the HMR socket runs on, defaults to a random free port (0 in node.js resolves to a random free port)
-  sourceMaps: true, // Enable or disable sourcemaps, defaults to enabled (minified builds currently always create sourcemaps)
+  sourceMaps: isDevMode, // Enable or disable sourcemaps, defaults to enabled (minified builds currently always create sourcemaps)
   hmrHostname: "" // A hostname for hot module reload, default to ''
   // detailedReport: false // Prints a detailed report of the bundles, assets, filesizes and times, defaults to false, reports are only printed if watch is disabled
 };
@@ -32,15 +33,15 @@ const optionsWin = {
   outDir: "./dist/windows", // The out directory to put the build files in, defaults to dist
   // outFile: 'index.html', // The name of the outputFile
   publicUrl: "./", // The url to serve on, defaults to '/'
-  watch: true, // Whether to watch the files and rebuild them on change, defaults to process.env.NODE_ENV !== 'production'
-  cache: true, // Enabled or disables caching, defaults to true
+  watch: isDevMode, // Whether to watch the files and rebuild them on change, defaults to process.env.NODE_ENV !== 'production'
+  cache: isDevMode, // Enabled or disables caching, defaults to true
   cacheDir: ".parcelCache", // The directory cache gets put in, defaults to .cache
   contentHash: false, // Disable content hash from being included on the filename
   target: "electron", // Browser/node/electron, defaults to browser
   // logLevel: 3, // 5 = save everything to a file, 4 = like 3, but with timestamps and additionally log http requests to dev server, 3 = log info, warnings & errors, 2 = log warnings & errors, 1 = log errors
-  hmr: true, // Enable or disable HMR while watching
+  hmr: isDevMode, // Enable or disable HMR while watching
   hmrPort: 0, // The port the HMR socket runs on, defaults to a random free port (0 in node.js resolves to a random free port)
-  sourceMaps: true, // Enable or disable sourcemaps, defaults to enabled (minified builds currently always create sourcemaps)
+  sourceMaps: isDevMode, // Enable or disable sourcemaps, defaults to enabled (minified builds currently always create sourcemaps)
   hmrHostname: "" // A hostname for hot module reload, default to ''
   // detailedReport: false // Prints a detailed report of the bundles, assets, filesizes and times, defaults to false, reports are only printed if watch is disabled
 };
@@ -50,15 +51,15 @@ const optionsHelp = {
   outDir: "./dist/windows/help", // The out directory to put the build files in, defaults to dist
   // outFile: 'index.html', // The name of the outputFile
   publicUrl: "./", // The url to serve on, defaults to '/'
-  watch: true, // Whether to watch the files and rebuild them on change, defaults to process.env.NODE_ENV !== 'production'
-  cache: true, // Enabled or disables caching, defaults to true
+  watch: isDevMode, // Whether to watch the files and rebuild them on change, defaults to process.env.NODE_ENV !== 'production'
+  cache: isDevMode, // Enabled or disables caching, defaults to true
   cacheDir: ".parcelCache", // The directory cache gets put in, defaults to .cache
   contentHash: false, // Disable content hash from being included on the filename
   target: "electron", // Browser/node/electron, defaults to browser
   // logLevel: 3, // 5 = save everything to a file, 4 = like 3, but with timestamps and additionally log http requests to dev server, 3 = log info, warnings & errors, 2 = log warnings & errors, 1 = log errors
-  hmr: true, // Enable or disable HMR while watching
+  hmr: isDevMode, // Enable or disable HMR while watching
   hmrPort: 0, // The port the HMR socket runs on, defaults to a random free port (0 in node.js resolves to a random free port)
-  sourceMaps: true, // Enable or disable sourcemaps, defaults to enabled (minified builds currently always create sourcemaps)
+  sourceMaps: isDevMode, // Enable or disable sourcemaps, defaults to enabled (minified builds currently always create sourcemaps)
   hmrHostname: "" // A hostname for hot module reload, default to ''
   // detailedReport: false // Prints a detailed report of the bundles, assets, filesizes and times, defaults to false, reports are only printed if watch is disabled
 };
@@ -68,20 +69,25 @@ const optionsStyles = {
   outDir: "./dist/styles", // The out directory to put the build files in, defaults to dist
   // outFile: 'index.html', // The name of the outputFile
   publicUrl: "./", // The url to serve on, defaults to '/'
-  watch: true, // Whether to watch the files and rebuild them on change, defaults to process.env.NODE_ENV !== 'production'
-  cache: true, // Enabled or disables caching, defaults to true
+  watch: isDevMode, // Whether to watch the files and rebuild them on change, defaults to process.env.NODE_ENV !== 'production'
+  cache: isDevMode, // Enabled or disables caching, defaults to true
   cacheDir: ".parcelCache", // The directory cache gets put in, defaults to .cache
   contentHash: false, // Disable content hash from being included on the filename
   target: "electron", // Browser/node/electron, defaults to browser
   // logLevel: 3, // 5 = save everything to a file, 4 = like 3, but with timestamps and additionally log http requests to dev server, 3 = log info, warnings & errors, 2 = log warnings & errors, 1 = log errors
-  hmr: true, // Enable or disable HMR while watching
+  hmr: isDevMode, // Enable or disable HMR while watching
   hmrPort: 0, // The port the HMR socket runs on, defaults to a random free port (0 in node.js resolves to a random free port)
-  sourceMaps: true, // Enable or disable sourcemaps, defaults to enabled (minified builds currently always create sourcemaps)
+  sourceMaps: isDevMode, // Enable or disable sourcemaps, defaults to enabled (minified builds currently always create sourcemaps)
   hmrHostname: "" // A hostname for hot module reload, default to ''
   // detailedReport: false // Prints a detailed report of the bundles, assets, filesizes and times, defaults to false, reports are only printed if watch is disabled
 };
 
 (async function() {
+  if (!isDevMode) {
+    // If running prod build clear dist before rebuilding
+    await fs.remove(__dirname + "/dist");
+  }
+
   // Initializes a bundler using the entrypoint location and options provided
   const bundler = new Bundler(entryFiles, options);
   const bundlerWin = new Bundler(entryFilesWin, optionsWin);
@@ -95,30 +101,3 @@ const optionsStyles = {
   const bundleHelp = await bundlerHelp.bundle();
   const bundleStyles = await bundlerStyles.bundle();
 })();
-
-/*
-parcel build src/index.js
-  --target=electron
-  --no-content-hash
-  --out-dir dist
-  --cache-dir .parcelCache
-  --no-source-maps;
-
-parcel build src/windows/*.html
-  --target=electron
-  --no-content-hash
-  --public-url ./
-  --out-dir dist/windows
-  --cache-dir .parcelCache
-  --no-source-maps;
-
-parcel build src/windows/help/*.html
-  --target=electron
-  --no-content-hash
-  --public-url ./
-  --out-dir dist/windows/help
-  --cache-dir .parcelCache
-  --no-source-maps;
-
-mkdir -p dist/styles; cp -r src/styles/theme* dist/styles/",
-*/
