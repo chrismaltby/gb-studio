56cbc7f20ee161a9060d0477a307438ee689b96e
Chris Maltby <chris@feed.xyz>
2019-03-26 14:36:48 +0000

    Input picker controllable with keyboard

--- src/components/forms/InputPicker.js
+++ src/components/forms/InputPicker.js
@@ -50,35 +50,48 @@ class InputPicker extends Component {
 
     return (
       <div className="InputPicker">
-        {inputs.map(input => (
-          <div
-            key={input.key}
-            onClick={() => {
-              if (Array.isArray(value)) {
-                if (value.indexOf(input.key) > -1) {
-                  onChange(value.filter(i => i !== input.key));
-                } else {
-                  onChange([].concat(value, input.key));
-                }
-              } else {
-                onChange([input.key]);
-              }
-            }}
-            className={cx(
-              "InputPicker__Button",
-              "InputPicker__Button--" + input.name,
-              {
-                "InputPicker__Button--Active":
-                  value && value.indexOf && value.indexOf(input.key) > -1
-              }
-            )}
-          >
-            {input.label}
-          </div>
-        ))}
+        <div className="InputPicker__Row">
+          {inputs.slice(0, 4).map(renderButton(value, onChange))}
+        </div>
+        <div className="InputPicker__Row">
+          {inputs.slice(4, 8).map(renderButton(value, onChange))}
+        </div>
       </div>
     );
   }
 }
+
+const renderButton = (value, onChange) => input => (
+  <label key={input.key} title={input.name}>
+    <input
+      type="checkbox"
+      checked={value && value.indexOf && value.indexOf(input.key) > -1}
+      onChange={() => {
+        if (Array.isArray(value)) {
+          if (value.indexOf(input.key) > -1) {
+            onChange(value.filter(i => i !== input.key));
+          } else {
+            onChange([].concat(value, input.key));
+          }
+        } else {
+          onChange([input.key]);
+        }
+      }}
+    />
+    <div
+      key={input.key}
+      className={cx(
+        "InputPicker__Button",
+        "InputPicker__Button--" + input.name,
+        {
+          "InputPicker__Button--Active":
+            value && value.indexOf && value.indexOf(input.key) > -1
+        }
+      )}
+    >
+      {input.label}
+    </div>
+  </label>
+);
 
 export default InputPicker;
