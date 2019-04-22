2554bf4ba7375dce5aeaed5a9d1cffede2020130
chrismaltby <chris.maltby@gmail.com>
2019-04-22 20:44:34 +0100

    Ignore .DS_Store files and filter any out within appData and buildTools before making distributables

--- after-copy.js
+++ after-copy.js
@@ -1,4 +1,12 @@
 const fs = require("fs-extra");
+const Path = require("path");
+
+const disallowedFiles = [".DS_Store"];
+
+function fileFilter(src, dest) {
+  const filename = Path.basename(src);
+  return disallowedFiles.indexOf(filename) === -1;
+}
 
 function afterCopy(buildPath, electronVersion, platform, arch, callback) {
   // Called from electronPackagerConfig in package.json
@@ -6,9 +14,11 @@ function afterCopy(buildPath, electronVersion, platform, arch, callback) {
   const toolsDir = "/buildTools/" + platform + "-" + arch;
   const dataDir = "/appData/";
 
-  fs.copy(__dirname + toolsDir, buildPath + toolsDir)
+  fs.copy(__dirname + toolsDir, buildPath + toolsDir, { filter: fileFilter })
     .then(() => {
-      return fs.copy(__dirname + dataDir, buildPath + dataDir);
+      return fs.copy(__dirname + dataDir, buildPath + dataDir, {
+        filter: fileFilter
+      });
     })
     .then(() => callback())
     .catch(err => callback(err));
