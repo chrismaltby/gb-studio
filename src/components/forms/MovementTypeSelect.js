7f4c6e23af89b763860a6e43cda96b3bbbc58692
Chris Maltby <chris.maltby@gmail.com>
2019-05-01 14:23:12 +0100

    Added l10n for movement selector

--- src/components/forms/MovementTypeSelect.js
+++ src/components/forms/MovementTypeSelect.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
+import l10n from "../../lib/helpers/l10n";
 
 class MovementTypeSelect extends Component {
   render() {
     return (
       <select {...this.props}>
-        <option value="static">Static</option>
-        <option value="faceInteraction">Face Interaction</option>
-        <option value="randomFace">Random Rotation</option>
-        <option value="randomWalk">Random Walk</option>
+        <option value="static">{l10n("FIELD_MOVEMENT_STATIC")}</option>
+        <option value="faceInteraction">
+          {l10n("FIELD_MOVEMENT_FACE_INTERACTION")}
+        </option>
+        <option value="randomFace">
+          {l10n("FIELD_MOVEMENT_RANDOM_ROTATION")}
+        </option>
+        <option value="randomWalk">{l10n("FIELD_MOVEMENT_RANDOM_WALK")}</option>
       </select>
     );
   }
