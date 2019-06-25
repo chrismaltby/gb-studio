2ec8619a59af0c86153bcbb7b1d0fed208c741d0
chrismaltby <chris.maltby@gmail.com>
2019-06-25 20:53:40 +0100

    Check for .itch or too long tmp path and fallback to shorter ones where needed. Create tmp dir if doesn't exist

--- src/lib/helpers/getTmp.js
+++ src/lib/helpers/getTmp.js
@@ -1,12 +1,17 @@
 import { remote } from "electron";
+import fs from "fs-extra";
 
 export default () => {
-  const tmpPath = remote.app.getPath("temp");
-  if (tmpPath.indexOf(" ") === -1) {
-    return tmpPath;
-  }
-  if (process.platform === "win32") {
-    return "C:\tmp";
-  }
-  return "/tmp";
+  let tmpPath = remote.app.getPath("temp");
+  if (
+    tmpPath.indexOf(" ") === -1 &&
+    tmpPath.indexOf(".itch") === -1 &&
+    (process.platform !== "win32" || tmpPath.length < 35)
+  ) {
+    // Ok
+  } else if (process.platform === "win32") {
+    tmpPath = "C:\\tmp";
+  } else tmpPath = "/tmp";
+  fs.ensureDirSync(tmpPath);
+  return tmpPath;
 };
