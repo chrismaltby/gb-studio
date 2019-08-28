683b6d2ca6a57004fc13191c25cc76f15938cf12
Manuel Illmayer <manil@ooev.at>
2019-08-28 08:09:30 +0200

    edit german local

--- src/lib/compiler/eventTypes.js
+++ src/lib/compiler/eventTypes.js
@@ -33,7 +33,6 @@ export const EVENT_CHOICE = "EVENT_CHOICE";
 export const EVENT_RESET_VARIABLES = "EVENT_RESET_VARIABLES";
 export const EVENT_LOOP = "EVENT_LOOP";
 export const EVENT_GROUP = "EVENT_GROUP";
-export const EVENT_COMMENT = "EVENT_COMMENT";
 
 // Background Scripts
 export const EVENT_SET_INPUT_SCRIPT = "EVENT_SET_INPUT_SCRIPT";
@@ -131,7 +130,6 @@ export const EventFields = {
     }
   ],
   [EVENT_GROUP]: [],
-  [EVENT_COMMENT]: [],
   [EVENT_SET_INPUT_SCRIPT]: [
     {
       key: "input",
