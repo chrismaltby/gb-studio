a63866ddfdb64d8b7b3df0111a014b5339ba9eb5
Chris Maltby <chris.maltby@gmail.com>
2019-09-02 14:10:49 +0100

    Fix l10n warning on application startup

--- src/lib/electron/dialog/switchLanguageDialog.js
+++ src/lib/electron/dialog/switchLanguageDialog.js
@@ -1,9 +1,10 @@
 import electron from "electron";
-import l10n from "../../helpers/l10n";
 
 const dialog = electron.remote ? electron.remote.dialog : electron.dialog;
 
 export default () => {
+  // eslint-disable-next-line global-require
+  const l10n = require("../../helpers/l10n").default;
   const dialogOptions = {
     type: "info",
     buttons: [l10n("DIALOG_OK")],
