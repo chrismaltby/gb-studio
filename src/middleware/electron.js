c0e15291d32133b48392acd7acbab0906f7dbedd
Chris Maltby <chris@feed.xyz>
2019-10-24 22:02:12 +0100

    Display confirmation message if deleting a custom event that has been used in a scene/actor/trigger. Deleting the custom event will also delete all instances of the event

--- src/middleware/electron.js
+++ src/middleware/electron.js
@@ -1,6 +1,7 @@
 import { ipcRenderer, clipboard, remote } from "electron";
 import settings from "electron-settings";
 import open from "open";
+import { uniq } from "lodash";
 import {
   OPEN_HELP,
   OPEN_FOLDER,
@@ -12,8 +13,15 @@ import {
   COPY_SCRIPT,
   SIDEBAR_WORLD_RESIZE,
   SIDEBAR_FILES_RESIZE,
-  PROJECT_LOAD_FAILURE
+  PROJECT_LOAD_FAILURE,
+  REMOVE_PROCEDURE
 } from "../actions/actionTypes";
+import confirmDeleteCustomEvent from "../lib/electron/dialog/confirmDeleteCustomEvent";
+import { getScenes, getScenesLookup, getProcedures, getActorsLookup, getTriggersLookup } from "../reducers/entitiesReducer";
+import { walkEvents, filterEvents } from "../lib/helpers/eventSystem";
+import { EVENT_CALL_PROCEDURE } from "../lib/compiler/eventTypes";
+import { editScene, editActor, editTrigger } from "../actions";
+import l10n from "../lib/helpers/l10n";
 
 export default store => next => action => {
   if (action.type === OPEN_HELP) {
@@ -89,7 +97,92 @@ export default store => next => action => {
   } else if (action.type === PROJECT_LOAD_FAILURE) {
     const window = remote.getCurrentWindow();
     window.close();
+  } else if (action.type === REMOVE_PROCEDURE) {
+    const state = store.getState();
+    const procedure =
+      state.entities.present.entities.procedures[action.procedureId];
+    const procedureIndex = getProcedures(state).indexOf(procedure);
+    const procedureName = procedure.name || `${l10n("CUSTOM_EVENT")} ${procedureIndex + 1}`
+    const scenes = getScenes(state);
+    const scenesLookup = getScenesLookup(state);
+    const actorsLookup = getActorsLookup(state);
+    const triggersLookup = getTriggersLookup(state);
+    const usedScenes = [];
+    const usedActors = [];
+    const usedTriggers = [];
+
+    const isThisEvent = (event) =>
+      event.command === EVENT_CALL_PROCEDURE &&
+      event.args.procedure === action.procedureId;
+
+    const sceneName = (sceneId) => {
+      const scene = scenesLookup[sceneId];
+      const sceneIndex = scenes.indexOf(scene);
+      return scene.name || `${l10n("SCENE")} ${sceneIndex + 1}`
+    }
+
+    // Check for uses of this custom event in project
+    scenes.forEach(scene => {
+      walkEvents(scene.script, event => {
+        if (isThisEvent(event)) {
+          usedScenes.push([scene.id, event.id]);
+        }
+      });
+      scene.actors.forEach((actorId) => {
+        walkEvents(actorsLookup[actorId].script, event => {
+          if (isThisEvent(event)) {
+            usedActors.push([scene.id, actorId, event.id]);
+          }
+        });        
+      })
+      scene.triggers.forEach((triggerId) => {
+        walkEvents(triggersLookup[triggerId].script, event => {
+          if (isThisEvent(event)) {
+            usedTriggers.push([scene.id, triggerId, event.id]);
+          }
+        });        
+      })      
+    });
+
+    const usedTotal = usedScenes.length + usedActors.length + usedTriggers.length;
+
+    if (usedTotal > 0) {
+      const sceneNames = uniq([].concat([], usedScenes, usedActors, usedTriggers)
+        .map(([sceneId]) => sceneName(sceneId)))
+        .sort();
+      
+      // Display confirmation and stop delete if cancelled
+      const cancel = confirmDeleteCustomEvent(procedureName, sceneNames, usedTotal);
+      if (cancel) {
+        return;
+      }
+
+      // Remove used instances in scenes
+      usedScenes.forEach(([sceneId, eventId]) => {
+        store.dispatch(
+          editScene(sceneId, {
+            script: filterEvents(scenesLookup[sceneId].script, eventId)
+          })
+        );
+      });
+      // Remove used instances in actors
+      usedActors.forEach(([sceneId, actorId, eventId]) => {
+        store.dispatch(
+          editActor(sceneId, actorId, {
+            script: filterEvents(actorsLookup[actorId].script, eventId)
+          })
+        );
+      });    
+      // Remove used instances in triggers
+      usedTriggers.forEach(([sceneId, triggerId, eventId]) => {
+        store.dispatch(
+          editTrigger(sceneId, triggerId, {
+            script: filterEvents(triggersLookup[triggerId].script, eventId)
+          })
+        );
+      });            
+    }
   }
 
-  return next(action);
+  next(action);
 };
