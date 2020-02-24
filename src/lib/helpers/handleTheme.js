de9655cd958c46cdc66ead00c32a4563865c5a77
Chris Maltby <chris.maltby@gmail.com>
2020-02-24 07:35:48 +0000

    Fix warnings when handling system theme

--- src/lib/helpers/handleTheme.js
+++ src/lib/helpers/handleTheme.js
@@ -3,29 +3,32 @@ import { ipcRenderer } from "electron";
 import darkTheme from "!!raw-loader!../../styles/theme-dark.css";
 import lightTheme from "!!raw-loader!../../styles/theme.css";
 
-const { systemPreferences } = require("electron").remote;
+const { nativeTheme } = require("electron").remote;
 
-function updateMyAppTheme() {
+const updateMyAppTheme = () => {
+  const settingsTheme = settings.get("theme");
+  const theme =
+    settingsTheme === "dark"
+      ? "dark"
+      : settingsTheme === "light"
+      ? "light"
+      : "system";
+
   const darkMode =
-    settings.get("theme") === "dark" ||
-    (settings.get("theme") === undefined &&
-      systemPreferences.isDarkMode &&
-      systemPreferences.isDarkMode());
+    theme === "dark" || (theme === "system" && nativeTheme.shouldUseDarkColors);
+
   const themeStyle = document.getElementById("theme");
   themeStyle.innerHTML = darkMode ? darkTheme : lightTheme;
+
+  nativeTheme.themeSource = theme;
 }
 
-if (systemPreferences.subscribeNotification) {
-  systemPreferences.subscribeNotification(
-    "AppleInterfaceThemeChangedNotification",
-    function theThemeHasChanged() {
-      updateMyAppTheme();
-    }
-  );
-}
-
-updateMyAppTheme();
+nativeTheme.on("updated", () => {
+  updateMyAppTheme();
+});
 
 ipcRenderer.on("update-theme", () => {
   updateMyAppTheme();
 });
+
+updateMyAppTheme();
