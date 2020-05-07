e64d09eaa2c6467083f44ba1fed412460659fa50
Chris Maltby <chris.maltby@gmail.com>
2020-05-07 17:54:59 +0100

    Fix console page on electron 8

--- src/containers/pages/BuildPage.js
+++ src/containers/pages/BuildPage.js
@@ -50,7 +50,7 @@ class BuildPage extends Component {
       <div
         style={{
           width: "100%",
-          height: "100%",
+          height: "calc(100vh - 38px)",
           display: "flex",
           flexDirection: "column"
         }}
