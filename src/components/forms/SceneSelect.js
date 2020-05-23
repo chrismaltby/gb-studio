9735bb6e992edffce918b08502ddadb0d6bb81e3
Chris Maltby <chris.maltby@gmail.com>
2020-05-23 18:52:36 +0100

    Scene Select using redux connections on Option and Dropdown to reduce unnecessary rerenders

--- src/components/forms/SceneSelect.js
+++ src/components/forms/SceneSelect.js
@@ -2,102 +2,109 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Select, { components } from "react-select";
 import { connect } from "react-redux";
-import memoize from "lodash/memoize";
 import { SceneShape, BackgroundShape } from "../../reducers/stateShape";
 import { assetFilename } from "../../lib/helpers/gbstudio";
-import { getScenesLookup, getScenes, getBackgroundsLookup } from "../../reducers/entitiesReducer";
+import {
+  getScenesLookup,
+  getBackgroundsLookup,
+  getSceneIds,
+} from "../../reducers/entitiesReducer";
 
-const getCachedObject = memoize(t => t, JSON.stringify);
-
-const DropdownIndicator = ({
-  scenesLookup,
-  backgroundsLookup,
-  projectRoot,
-  value
-}) => props => {
+const DropdownIndicator = ({ filename, ...props }) => {
+  console.log("RENDER DropdownIndicator ")
   return (
     <components.DropdownIndicator {...props}>
-      {value &&
-        scenesLookup[value] &&
-        backgroundsLookup[scenesLookup[value].backgroundId] && (
-          <div
-            className="Thumbnail"
-            style={{
-              backgroundImage: `url("file://${assetFilename(
-                projectRoot,
-                "backgrounds",
-                backgroundsLookup[scenesLookup[value].backgroundId]
-              )}?_v=${backgroundsLookup[scenesLookup[value].backgroundId]._v}")`
-            }}
-          />
-        )}
+      {filename && (
+        <div
+          className="Thumbnail"
+          style={{
+            backgroundImage: `url("file://${filename}")`,
+          }}
+        />
+      )}
     </components.DropdownIndicator>
   );
 };
 
-const Option = ({ scenesLookup, backgroundsLookup, projectRoot }) => props => {
-  const { value, label } = props;
+DropdownIndicator.propTypes = {
+  filename: PropTypes.string.isRequired,
+};
+
+const DropdownIndicatorWithData = (value) =>
+  connect((state) => {
+    const projectRoot = state.document && state.document.root;
+    const scenesLookup = getScenesLookup(state);
+    const backgroundsLookup = getBackgroundsLookup(state);
+    const filename =
+      scenesLookup[value] &&
+      backgroundsLookup[scenesLookup[value].backgroundId] &&
+      `${assetFilename(
+        projectRoot,
+        "backgrounds",
+        backgroundsLookup[scenesLookup[value].backgroundId]
+      )}?_v=${backgroundsLookup[scenesLookup[value].backgroundId]._v}`;
+    return {
+      filename,
+    };
+  })(DropdownIndicator);
+
+const Option = ({ label, value, filename, ...props }) => {
+  console.log("RENDER Option ")
+
   return (
     <components.Option {...props}>
       <div style={{ display: "flex" }}>
         <div style={{ flexGrow: 1 }}>{label}</div>
-        {scenesLookup[value] &&
-          backgroundsLookup[scenesLookup[value].backgroundId] && (
-            <div
-              className="Thumbnail"
-              style={{
-                backgroundImage: `url("file://${assetFilename(
-                  projectRoot,
-                  "backgrounds",
-                  backgroundsLookup[scenesLookup[value].backgroundId]
-                )}?_v=${
-                  backgroundsLookup[scenesLookup[value].backgroundId]._v
-                }")`
-              }}
-            />
-          )}
+        {filename && (
+          <div
+            className="Thumbnail"
+            style={{
+              backgroundImage: `url("file://${filename}")`,
+            }}
+          />
+        )}
       </div>
     </components.Option>
   );
 };
 
-class SceneSelect extends Component {
+Option.propTypes = {
+  label: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  filename: PropTypes.string.isRequired
+};
 
-  render() {
-    const {
-      allowNone,
-      scenes,
-      id,
-      value,
-      onChange,
-      backgroundsLookup,
-      scenesLookup,
-      projectRoot
-    } = this.props;
-    const current = scenes.find(m => m.id === value);
-
-    const options = [].concat(
-      allowNone
-        ? {
-            value: "",
-            label: "None"
-          }
-        : [],
-      scenes.map((scene, index) => {
-        return {
-          value: scene.id,
-          label: scene.name || `Scene ${index + 1}`
-        };
-      })
-    );
-
-    const MyDropdownIndicator = DropdownIndicator({
-      backgroundsLookup,
-      scenesLookup,
+const OptionWithData = connect((state, ownProps) => {
+  const projectRoot = state.document && state.document.root;
+  const scenesLookup = getScenesLookup(state);
+  const backgroundsLookup = getBackgroundsLookup(state);
+  const { value, label: sceneIndex } = ownProps;
+  const scene = scenesLookup[value];
+  const label = scene.name || `Scene ${sceneIndex + 1}`;
+  const filename =
+    scenesLookup[value] &&
+    backgroundsLookup[scenesLookup[value].backgroundId] &&
+    `${assetFilename(
       projectRoot,
-      value
-    });
-    const MyOption = Option({ backgroundsLookup, scenesLookup, projectRoot });
+      "backgrounds",
+      backgroundsLookup[scenesLookup[value].backgroundId]
+    )}?_v=${backgroundsLookup[scenesLookup[value].backgroundId]._v}`;
+  return {
+    label,
+    filename,
+  };
+})(Option);
+
+class SceneSelect extends Component {
+  render() {
+    console.log("RENDER SceneSelect ")
+
+    const { sceneIds, id, label, value, onChange } = this.props;
+
+    const options = sceneIds.map((sceneId, sceneIndex) => ({
+      value: sceneId,
+      label: sceneIndex,
+    }));
 
     return (
       <Select
@@ -105,67 +112,42 @@ class SceneSelect extends Component {
         className="ReactSelectContainer"
         classNamePrefix="ReactSelect"
         options={options}
-        value={{ label: current ? current.name : "", value }}
-        onChange={data => {
+        value={{ label }}
+        onChange={(data) => {
           onChange(data.value);
         }}
         components={{
-          DropdownIndicator: MyDropdownIndicator,
-          Option: MyOption
+          DropdownIndicator: DropdownIndicatorWithData(value),
+          Option: OptionWithData,
         }}
       />
     );
   }
 }
 
-export const CachedSceneShape = PropTypes.shape({
-  id: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  backgroundId: PropTypes.string.isRequired
-});
-
-export const CachedSceneLookupShape = PropTypes.shape({
-  backgroundId: PropTypes.string.isRequired
-});
-
 SceneSelect.propTypes = {
   id: PropTypes.string,
   value: PropTypes.string,
+  label: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
-  allowNone: PropTypes.bool,
-  scenes: PropTypes.arrayOf(CachedSceneShape).isRequired,
-  scenesLookup: PropTypes.objectOf(CachedSceneLookupShape).isRequired,
-  backgroundsLookup: PropTypes.objectOf(BackgroundShape).isRequired,
-  projectRoot: PropTypes.string.isRequired
+  sceneIds: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
 };
 
 SceneSelect.defaultProps = {
   id: undefined,
   value: "",
-  allowNone: false
 };
 
-function mapStateToProps(state) {
-  const fullScenesLookup = getScenesLookup(state);
-  const fullScenes = getScenes(state);
-  const scenesLookup = getCachedObject(Object.keys(fullScenesLookup).reduce((memo, key) => {
-    // eslint-disable-next-line no-param-reassign
-    memo[key] = fullScenesLookup[key] && {
-      backgroundId: fullScenesLookup[key].backgroundId
-    };
-    return memo;
-  }, {}));
-  const scenes = getCachedObject(fullScenes.map((scene) => ({
-    id: scene.id,
-    name: scene.name,
-    backgroundId: scene.backgroundId
-  })));
-  const backgroundsLookup = getBackgroundsLookup(state);
+function mapStateToProps(state, ownProps) {
+  const scenesLookup = getScenesLookup(state);
+  const sceneIds = getSceneIds(state);
+  const sceneIndex = sceneIds.indexOf(ownProps.value);
+  const scene = scenesLookup[ownProps.value];
+  const label = scene ? scene.name || `Scene ${sceneIndex + 1}` : "";
+
   return {
-    backgroundsLookup,
-    scenesLookup,
-    scenes,
-    projectRoot: state.document && state.document.root
+    sceneIds,
+    label,
   };
 }
 
