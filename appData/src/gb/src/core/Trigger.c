2085557538997c31d155046cd8fde7edcb11acba
Chris Maltby <chris.maltby@gmail.com>
2020-06-14 13:22:29 +0100

    Move trigger at check into banked function to reduce code causing overflow when colors enabled

--- appData/src/gb/src/core/Trigger.c
+++ appData/src/gb/src/core/Trigger.c
@@ -1,5 +1,6 @@
 #include "Trigger.h"
 
+#include "BankManager.h"
 #include "ScriptRunner.h"
 
 Trigger triggers[MAX_TRIGGERS];
@@ -7,27 +8,14 @@ UBYTE triggers_active[MAX_ACTIVE_TRIGGERS];
 UBYTE triggers_active_size;
 UBYTE triggers_len;
 
+UBYTE TriggerAtTile_b(UBYTE tx_a, UBYTE ty_a);
+
 UBYTE TriggerAtTile(UBYTE tx_a, UBYTE ty_a) {
-  UBYTE i, tx_b, ty_b, tx_c, ty_c;
-
-  for (i = 0; i != triggers_len; i++) {
-    tx_b = triggers[i].x;
-    ty_b = triggers[i].y;
-    tx_c = tx_b + triggers[i].w - 1;
-    ty_c = ty_b + triggers[i].h - 1;
-
-    LOG("CHECK [%u,%u] tx_b=%u ty_b=%u tx_c=%u ty_c=%u\n", tx_a, ty_a, tx_b, ty_b, tx_c, ty_c);
-
-#ifdef COLS_16
-    if ((tx_a + 1) >= tx_b && tx_a <= tx_c && ty_a >= ty_b && (ty_a - 1) <= ty_c) {
-#else
-    if ((tx_a + 1) >= tx_b && tx_a <= tx_c && ty_a >= ty_b && ty_a <= ty_c) {
-#endif
-      return i;
-    }
-  }
-
-  return MAX_TRIGGERS;
+  UBYTE val;
+  PUSH_BANK(TRIGGER_BANK);
+  val = TriggerAtTile_b(tx_a, ty_a);
+  POP_BANK;
+  return val;
 }
 
 void TriggerRunScript(UBYTE i) {
---
+++ appData/src/gb/src/core/Trigger_b.c
@@ -0,0 +1,27 @@
+// clang-format off
+#pragma bank=1
+// clang-format on
+#include "Trigger.h"
+
+UBYTE TriggerAtTile_b(UBYTE tx_a, UBYTE ty_a) {
+  UBYTE i, tx_b, ty_b, tx_c, ty_c;
+
+  for (i = 0; i != triggers_len; i++) {
+    tx_b = triggers[i].x;
+    ty_b = triggers[i].y;
+    tx_c = tx_b + triggers[i].w - 1;
+    ty_c = ty_b + triggers[i].h - 1;
+
+    LOG("CHECK [%u,%u] tx_b=%u ty_b=%u tx_c=%u ty_c=%u\n", tx_a, ty_a, tx_b, ty_b, tx_c, ty_c);
+
+#ifdef COLS_16
+    if ((tx_a + 1) >= tx_b && tx_a <= tx_c && ty_a >= ty_b && (ty_a - 1) <= ty_c) {
+#else
+    if ((tx_a + 1) >= tx_b && tx_a <= tx_c && ty_a >= ty_b && ty_a <= ty_c) {
+#endif
+      return i;
+    }
+  }
+
+  return MAX_TRIGGERS;
+}
