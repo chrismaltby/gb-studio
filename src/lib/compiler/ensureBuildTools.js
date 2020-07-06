9a4764035f0f27742510f1fac1c3db196e6c175a
Chris Maltby <chris.maltby@gmail.com>
2020-07-06 17:39:04 +0100

    Add version to build tools, if local version differs replace your existing version with new version

--- src/lib/compiler/ensureBuildTools.js
+++ src/lib/compiler/ensureBuildTools.js
@@ -1,19 +1,30 @@
 import fs from "fs-extra";
+import rimraf from "rimraf";
+import { promisify } from "util";
 import { buildToolsRoot } from "../../consts";
 import copy from "../helpers/fsCopy";
 import getTmp from "../helpers/getTmp";
+
+const rmdir = promisify(rimraf);
+
+const EXPECTED_TOOLS_VERSION = "2.0.0";
 
 const ensureBuildTools = async () => {
   const buildToolsPath = `${buildToolsRoot}/${process.platform}-${process.arch}`;
 
   const tmpPath = getTmp();
   const tmpBuildToolsPath = `${tmpPath}/_gbs`;
+  const tmpBuildToolsVersionPath = `${tmpPath}/_gbs/tools_version`;
 
   // Symlink build tools so that path doesn't contain any spaces
   // GBDKDIR doesn't work if path has spaces :-(
   try {
-    await fs.fstat(tmpBuildToolsPath);
+    const toolsVersion = await fs.readFile(tmpBuildToolsVersionPath, "utf8");
+    if(toolsVersion !== EXPECTED_TOOLS_VERSION) {
+      throw new Error("Incorrect tools version found");
+    }
   } catch (e) {
+    await rmdir(tmpBuildToolsPath);
     await copy(buildToolsPath, tmpBuildToolsPath, {
       overwrite: false,
       mode: 0o755,
