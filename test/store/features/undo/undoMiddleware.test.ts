266c60d78375d11b6b750a3ec95b5a75c9e7eda7
Chris Maltby <chris.maltby@gmail.com>
2020-09-09 21:33:05 +0100

    Undo middleware tests

---
+++ test/store/features/undo/undoMiddleware.test.ts
@@ -0,0 +1,49 @@
+import "jest-extended";
+import middleware from "../../../../src/store/features/undo/undoMiddleware";
+import { RootState } from "../../../../src/store/configureStore";
+import { dummyProjectData } from "../../../dummydata";
+import { MiddlewareAPI, Dispatch, AnyAction } from "@reduxjs/toolkit";
+import projectActions from "../../../../src/store/features/project/projectActions";
+import { ActionCreators } from "redux-undo";
+
+test("Should trigger undo clear history after successful project load", async () => {
+  const store = ({
+    getState: () => ({}),
+    dispatch: jest.fn(),
+  } as unknown) as MiddlewareAPI<Dispatch<AnyAction>, RootState>;
+
+  const next = jest.fn();
+  const action = projectActions.loadProject.fulfilled(
+    {
+      data: { ...dummyProjectData },
+      path: "project.gbsproj",
+    },
+    "randomid",
+    "project.gbsproj"
+  );
+
+  middleware(store)(next)(action);
+
+  expect(store.dispatch).toHaveBeenCalledWith(ActionCreators.clearHistory());
+  expect(next).toHaveBeenCalledWith(action);
+  expect(next).toHaveBeenCalledBefore(store.dispatch as jest.Mock<any, any>);
+});
+
+test("Should not trigger undo clear history after successful project save", async () => {
+  const store = ({
+    getState: () => ({}),
+    dispatch: jest.fn(),
+  } as unknown) as MiddlewareAPI<Dispatch<AnyAction>, RootState>;
+
+  const next = jest.fn();
+  const action = projectActions.saveProject.fulfilled(
+    undefined,
+    "randomid",
+    undefined
+  );
+
+  middleware(store)(next)(action);
+
+  expect(store.dispatch).not.toHaveBeenCalled();
+  expect(next).toHaveBeenCalledWith(action);
+});
