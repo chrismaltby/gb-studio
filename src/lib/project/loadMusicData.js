c13a1e251e6a98c570936736086ed8f34fb55211
Chris Maltby <chris.maltby@gmail.com>
2020-09-28 21:50:59 +0100

    Allow renaming project assets while preserving id

--- src/lib/project/loadMusicData.js
+++ src/lib/project/loadMusicData.js
@@ -1,19 +1,22 @@
 import glob from "glob";
 import { promisify } from "util";
 import uuidv4 from "uuid/v4";
+import { stat } from "fs-extra";
 import parseAssetPath from "../helpers/path/parseAssetPath";
 
 const globAsync = promisify(glob);
 
 const loadMusicData = projectRoot => async filename => {
   const { file, plugin } = parseAssetPath(filename, projectRoot, "music");
-
+  const fileStat = await stat(filename, { bigint: true });
+  const inode = fileStat.ino.toString();
   return {
     id: uuidv4(),
     plugin,
     name: file.replace(/.mod/i, ""),
     filename: file,
     settings: {},
+    inode,
     _v: Date.now()
   };
 };
