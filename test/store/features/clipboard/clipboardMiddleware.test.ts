08b0ff56badead27b201da07e9ff0dc13d4905ae
Chris Maltby <chris.maltby@gmail.com>
2020-10-01 18:59:22 +0100

    Add test for adding actor variables to clipboard

--- test/store/features/clipboard/clipboardMiddleware.test.ts
+++ test/store/features/clipboard/clipboardMiddleware.test.ts
@@ -12,33 +12,106 @@ jest.mock("electron");
 const mockedClipboard = mocked(clipboard, true);
 
 test("Should be able to copy actor to clipboard", async () => {
+  mockedClipboard.writeText.mockClear();
 
   const store = ({
     getState: () => ({
-        project: {
-            present: {
-                entities: {
-                    customEvents: {
-                        entities: {},
-                        ids: []
-                    }
-                }
-            }
-        }
+      project: {
+        present: {
+          entities: {
+            customEvents: {
+              entities: {},
+              ids: [],
+            },
+            variables: {
+              entities: {},
+              ids: [],
+            },
+          },
+        },
+      },
     }),
     dispatch: jest.fn(),
   } as unknown) as MiddlewareAPI<Dispatch<AnyAction>, RootState>;
 
   const next = jest.fn();
   const action = actions.copyActor({
-      ...dummyActor
-  })
+    ...dummyActor,
+  });
 
   middleware(store)(next)(action);
 
   expect(next).toHaveBeenCalledWith(action);
-  expect(mockedClipboard.writeText).toHaveBeenCalledWith(JSON.stringify({
-      actor: dummyActor,
-      __type: "actor"
-  }, null, 4));
+  expect(mockedClipboard.writeText).toHaveBeenCalledWith(
+    JSON.stringify(
+      {
+        actor: dummyActor,
+        __type: "actor",
+      },
+      null,
+      4
+    )
+  );
+});
+
+test("Should be include referenced variables when copying actor", async () => {
+  mockedClipboard.writeText.mockClear();
+
+  const store = ({
+    getState: () => ({
+      project: {
+        present: {
+          entities: {
+            customEvents: {
+              entities: {},
+              ids: [],
+            },
+            variables: {
+              entities: {
+                actor1_L0: {
+                  id: "actor1_L0",
+                  name: "Actor Local",
+                },
+                actor2_L0: {
+                  id: "actor2_L0",
+                  name: "Actor Local",
+                },
+              },
+              ids: ["actor1_L0", "actor2_L0"],
+            },
+          },
+        },
+      },
+    }),
+    dispatch: jest.fn(),
+  } as unknown) as MiddlewareAPI<Dispatch<AnyAction>, RootState>;
+
+  const next = jest.fn();
+  const action = actions.copyActor({
+    ...dummyActor,
+    id: "actor1",
+  });
+
+  middleware(store)(next)(action);
+
+  expect(next).toHaveBeenCalledWith(action);
+  expect(mockedClipboard.writeText).toHaveBeenCalledWith(
+    JSON.stringify(
+      {
+        actor: {
+          ...dummyActor,
+          id: "actor1",
+        },
+        __type: "actor",
+        __variables: [
+          {
+            id: "actor1_L0",
+            name: "Actor Local",
+          },
+        ],
+      },
+      null,
+      4
+    )
+  );
 });
