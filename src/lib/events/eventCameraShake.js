7d6abb2cde83618d2df174aebfceccc837874aaa
Chris Maltby <chris.maltby@gmail.com>
2020-10-10 11:30:09 +0100

    Update event to use dropdown using patrickmollohan's implementation

--- src/lib/events/eventCameraShake.js
+++ src/lib/events/eventCameraShake.js
@@ -7,32 +7,49 @@ const fields = [
     key: "time",
     type: "number",
     label: l10n("FIELD_SECONDS"),
+    width: "50%",
     min: 0,
     max: 10,
     step: 0.1,
     defaultValue: 0.5
   },
   {
-    key: "shouldShakeX",
-    type: "checkbox",
-    label: l10n("FIELD_X"),
-    defaultValue: true
-  },
-  {
-    key: "shouldShakeY",
-    type: "checkbox",
-    label: l10n("FIELD_Y"),
-    defaultValue: false
+    key: "shakeDirection",
+    label: l10n("FIELD_DIRECTION"),
+    width: "50%",
+    type: "select",
+    options: [
+      ["horizontal", "↔ " + l10n("FIELD_HORIZONTAL")],
+      ["vertical", "↕ " + l10n("FIELD_VERTICAL")],
+      ["diagonal", "⤡ " + l10n("FIELD_DIAGONAL")]
+    ],
+    defaultValue: "horizontal"
   }
 ];
 
 const compile = (input, helpers) => {
   const { cameraShake } = helpers;
   let seconds = typeof input.time === "number" ? input.time : 0.5;
-  let shouldShakeX =
-    typeof input.shouldShakeX === "boolean" ? input.shouldShakeX : true;
-  let shouldShakeY =
-    typeof input.shouldShakeY === "boolean" ? input.shouldShakeY : false;
+  const shakeDirection = input.shakeDirection;
+  let shouldShakeX = true;
+  let shouldShakeY = false;
+  switch (shakeDirection) {
+  	case "horizontal":
+  		shouldShakeX = true;
+  		shouldShakeY = false;
+  		break;
+  	case "vertical":
+  		shouldShakeX = false;
+  		shouldShakeY = true;
+  		break;
+  	case "diagonal":
+  		shouldShakeX = true;
+  		shouldShakeY = true;
+  		break;
+  	default:
+  		shouldShakeX = true;
+  		shouldShakeY = false;
+  }
   // Convert seconds into frames (60fps)
   while (seconds > 0) {
     const time = Math.min(seconds, 1);
