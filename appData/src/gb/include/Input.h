chrismaltby/gb-studio

commit 410e6bec5d5b6f279d60fe41109410d5b4ef5bab

Attach script to button overrides default input
In 1.2 using "Attach script to button" disabled the default behavior for the button. So, attaching a script to A will disable the interaction with actors.

In 2.0 when attaching, the engine will try to run the two scripts in the background which lead to some unexpected issues like the player moving even if the directional buttons had an script attached ( #452 , #502 ).

This patch restores the 1.2 behavior.

appData/src/gb/include/Input.h
@@ -4,36 +4,44 @@
 #include <gb/gb.h>
 
 #include "BankData.h"
+#include "UI.h"
+
+#define ALLOW_DEFAULT_INPUT_RIGHT (ui_block || !GET_BIT(input_override_default,0))
+#define ALLOW_DEFAULT_INPUT_LEFT (ui_block || !GET_BIT(input_override_default,1))
+#define ALLOW_DEFAULT_INPUT_UP (ui_block || !GET_BIT(input_override_default,2))
+#define ALLOW_DEFAULT_INPUT_DOWN (ui_block || !GET_BIT(input_override_default,3))
+#define ALLOW_DEFAULT_INPUT_A (ui_block || !GET_BIT(input_override_default,4))
+#define ALLOW_DEFAULT_INPUT_B (ui_block || !GET_BIT(input_override_default,5))
 
 /* TRUE if left is being held on dpad */
-#define INPUT_LEFT (joy & J_LEFT)
+#define INPUT_LEFT ALLOW_DEFAULT_INPUT_LEFT && (joy & J_LEFT)
 
 /* TRUE if right is being held on dpad */
-#define INPUT_RIGHT (joy & J_RIGHT)
+#define INPUT_RIGHT ALLOW_DEFAULT_INPUT_RIGHT && (joy & J_RIGHT)
 
 /* TRUE if up is being held on dpad */
-#define INPUT_UP (joy & J_UP)
+#define INPUT_UP ALLOW_DEFAULT_INPUT_UP && (joy & J_UP)
 
 /* TRUE if down is being held on dpad */
-#define INPUT_DOWN (joy & J_DOWN)
+#define INPUT_DOWN ALLOW_DEFAULT_INPUT_DOWN && (joy & J_DOWN)
 
 /* TRUE if left is being held on dpad */
-#define INPUT_RECENT_LEFT ((recent_joy & J_LEFT) || (!recent_joy && (joy & J_LEFT)))
+#define INPUT_RECENT_LEFT ALLOW_DEFAULT_INPUT_LEFT && ((recent_joy & J_LEFT) || (!recent_joy && (joy & J_LEFT)))
 
 /* TRUE if right is being held on dpad */
-#define INPUT_RECENT_RIGHT ((recent_joy & J_RIGHT) || (!recent_joy && (joy & J_RIGHT)))
+#define INPUT_RECENT_RIGHT ALLOW_DEFAULT_INPUT_RIGHT && ((recent_joy & J_RIGHT) || (!recent_joy && (joy & J_RIGHT)))
 
 /* TRUE if up is being held on dpad */
-#define INPUT_RECENT_UP ((recent_joy & J_UP) || (!recent_joy && (joy & J_UP)))
+#define INPUT_RECENT_UP ALLOW_DEFAULT_INPUT_UP && ((recent_joy & J_UP) || (!recent_joy && (joy & J_UP)))
 
 /* TRUE if down is being held on dpad */
-#define INPUT_RECENT_DOWN ((recent_joy & J_DOWN) || (!recent_joy && (joy & J_DOWN)))
+#define INPUT_RECENT_DOWN ALLOW_DEFAULT_INPUT_DOWN && ((recent_joy & J_DOWN) || (!recent_joy && (joy & J_DOWN)))
 
 /* TRUE if A button is being held */
-#define INPUT_A (joy & J_A)
+#define INPUT_A ALLOW_DEFAULT_INPUT_A && (joy & J_A)
 
 /* TRUE if B button is being held */
-#define INPUT_B (joy & J_B)
+#define INPUT_B ALLOW_DEFAULT_INPUT_B && (joy & J_B)
 
 /* TRUE if Start button is being held */
 #define INPUT_START (joy & J_START)
@@ -42,22 +50,22 @@
 #define INPUT_SELECT (joy & J_SELECT)
 
 /* TRUE on first frame that left is pressed on dpad  */
-#define INPUT_LEFT_PRESSED ((joy & J_LEFT) && !(last_joy & J_LEFT))
+#define INPUT_LEFT_PRESSED ALLOW_DEFAULT_INPUT_LEFT && ((joy & J_LEFT) && !(last_joy & J_LEFT))
 
 /* TRUE on first frame that right is pressed on dpad  */
-#define INPUT_RIGHT_PRESSED ((joy & J_RIGHT) && !(last_joy & J_RIGHT))
+#define INPUT_RIGHT_PRESSED ALLOW_DEFAULT_INPUT_RIGHT && ((joy & J_RIGHT) && !(last_joy & J_RIGHT))
 
 /* TRUE on first frame that up is pressed on dpad  */
-#define INPUT_UP_PRESSED ((joy & J_UP) && !(last_joy & J_UP))
+#define INPUT_UP_PRESSED ALLOW_DEFAULT_INPUT_UP && ((joy & J_UP) && !(last_joy & J_UP))
 
 /* TRUE on first frame that down is pressed on dpad  */
-#define INPUT_DOWN_PRESSED ((joy & J_DOWN) && !(last_joy & J_DOWN))
+#define INPUT_DOWN_PRESSED ALLOW_DEFAULT_INPUT_DOWN && ((joy & J_DOWN) && !(last_joy & J_DOWN))
 
 /* TRUE on first frame that A button is pressed */
-#define INPUT_A_PRESSED ((joy & J_A) && !(last_joy & J_A))
+#define INPUT_A_PRESSED ALLOW_DEFAULT_INPUT_A && ((joy & J_A) && !(last_joy & J_A))
 
 /* TRUE on first frame that B button is pressed */
-#define INPUT_B_PRESSED ((joy & J_B) && !(last_joy & J_B))
+#define INPUT_B_PRESSED ALLOW_DEFAULT_INPUT_B && ((joy & J_B) && !(last_joy & J_B))
 
 /* TRUE on first frame that Start button is pressed */
 #define INPUT_START_PRESSED ((joy & J_START) && !(last_joy & J_START))
@@ -75,6 +83,7 @@ extern UBYTE recent_joy;
 extern UBYTE await_input;
 extern BankPtr input_script_ptrs[NUM_INPUTS];
 extern UBYTE input_script_persist;
+extern UBYTE input_override_default;
 
 /**
  * Check joypad values and run input scripts if required

appData/src/gb/src/core/Input.c
@@ -9,6 +9,7 @@ UBYTE await_input;
 UBYTE input_wait;
 BankPtr input_script_ptrs[NUM_INPUTS] = {{0}};
 UBYTE input_script_persist;
+UBYTE input_override_default;
 
 void HandleInputScripts() {
   UBYTE input_index, input_joy;

appData/src/gb/src/core/ScriptRunner_b.c
@@ -2008,6 +2008,8 @@ void Script_SetInputScript_b() {
     UNSET_BIT_MASK(input_script_persist, input);
   }
 
+  SET_BIT_MASK(input_override_default, input);
+
   index = 0;
   while (!(input & 1) && input != 0) {
     index += 1;
@@ -2027,6 +2029,8 @@ void Script_RemoveInputScript_b() {
   UBYTE input, index;
 
   input = script_cmd_args[0];
+
+  UNSET_BIT_MASK(input_override_default, input);
 
   index = 0;
   for (index = 0; index != 8; ++index) {

commit ecf0d186a0f4772a574577a6672ec8cf48a96de9

Fix movement in Adventure mode
Fox for #611 - Adventure scene type movement is broken

Regression introduced in 410e6bec5d5b6f279d60fe41109410d5b4ef5bab

appData/src/gb/engine_version
@@ -1,2 +1,2 @@
 # Don't remove this file, it is used to determine if your ejected engine is out of date.
-2.0.0-e8
+2.0.0-e9

appData/src/gb/include/Input.h
@@ -14,34 +14,34 @@
 #define ALLOW_DEFAULT_INPUT_B (ui_block || !GET_BIT(input_override_default,5))
 
 /* TRUE if left is being held on dpad */
-#define INPUT_LEFT ALLOW_DEFAULT_INPUT_LEFT && (joy & J_LEFT)
+#define INPUT_LEFT (ALLOW_DEFAULT_INPUT_LEFT && (joy & J_LEFT))
 
 /* TRUE if right is being held on dpad */
-#define INPUT_RIGHT ALLOW_DEFAULT_INPUT_RIGHT && (joy & J_RIGHT)
+#define INPUT_RIGHT (ALLOW_DEFAULT_INPUT_RIGHT && (joy & J_RIGHT))
 
 /* TRUE if up is being held on dpad */
-#define INPUT_UP ALLOW_DEFAULT_INPUT_UP && (joy & J_UP)
+#define INPUT_UP (ALLOW_DEFAULT_INPUT_UP && (joy & J_UP))
 
 /* TRUE if down is being held on dpad */
-#define INPUT_DOWN ALLOW_DEFAULT_INPUT_DOWN && (joy & J_DOWN)
+#define INPUT_DOWN (ALLOW_DEFAULT_INPUT_DOWN && (joy & J_DOWN))
 
 /* TRUE if left is being held on dpad */
-#define INPUT_RECENT_LEFT ALLOW_DEFAULT_INPUT_LEFT && ((recent_joy & J_LEFT) || (!recent_joy && (joy & J_LEFT)))
+#define INPUT_RECENT_LEFT (ALLOW_DEFAULT_INPUT_LEFT && ((recent_joy & J_LEFT) || (!recent_joy && (joy & J_LEFT))))
 
 /* TRUE if right is being held on dpad */
-#define INPUT_RECENT_RIGHT ALLOW_DEFAULT_INPUT_RIGHT && ((recent_joy & J_RIGHT) || (!recent_joy && (joy & J_RIGHT)))
+#define INPUT_RECENT_RIGHT (ALLOW_DEFAULT_INPUT_RIGHT && ((recent_joy & J_RIGHT) || (!recent_joy && (joy & J_RIGHT))))
 
 /* TRUE if up is being held on dpad */
-#define INPUT_RECENT_UP ALLOW_DEFAULT_INPUT_UP && ((recent_joy & J_UP) || (!recent_joy && (joy & J_UP)))
+#define INPUT_RECENT_UP (ALLOW_DEFAULT_INPUT_UP && ((recent_joy & J_UP) || (!recent_joy && (joy & J_UP))))
 
 /* TRUE if down is being held on dpad */
-#define INPUT_RECENT_DOWN ALLOW_DEFAULT_INPUT_DOWN && ((recent_joy & J_DOWN) || (!recent_joy && (joy & J_DOWN)))
+#define INPUT_RECENT_DOWN (ALLOW_DEFAULT_INPUT_DOWN && ((recent_joy & J_DOWN) || (!recent_joy && (joy & J_DOWN))))
 
 /* TRUE if A button is being held */
-#define INPUT_A ALLOW_DEFAULT_INPUT_A && (joy & J_A)
+#define INPUT_A (ALLOW_DEFAULT_INPUT_A && (joy & J_A))
 
 /* TRUE if B button is being held */
-#define INPUT_B ALLOW_DEFAULT_INPUT_B && (joy & J_B)
+#define INPUT_B (ALLOW_DEFAULT_INPUT_B && (joy & J_B))
 
 /* TRUE if Start button is being held */
 #define INPUT_START (joy & J_START)
@@ -50,22 +50,22 @@
 #define INPUT_SELECT (joy & J_SELECT)
 
 /* TRUE on first frame that left is pressed on dpad  */
-#define INPUT_LEFT_PRESSED ALLOW_DEFAULT_INPUT_LEFT && ((joy & J_LEFT) && !(last_joy & J_LEFT))
+#define INPUT_LEFT_PRESSED (ALLOW_DEFAULT_INPUT_LEFT && ((joy & J_LEFT) && !(last_joy & J_LEFT)))
 
 /* TRUE on first frame that right is pressed on dpad  */
-#define INPUT_RIGHT_PRESSED ALLOW_DEFAULT_INPUT_RIGHT && ((joy & J_RIGHT) && !(last_joy & J_RIGHT))
+#define INPUT_RIGHT_PRESSED (ALLOW_DEFAULT_INPUT_RIGHT && ((joy & J_RIGHT) && !(last_joy & J_RIGHT)))
 
 /* TRUE on first frame that up is pressed on dpad  */
-#define INPUT_UP_PRESSED ALLOW_DEFAULT_INPUT_UP && ((joy & J_UP) && !(last_joy & J_UP))
+#define INPUT_UP_PRESSED (ALLOW_DEFAULT_INPUT_UP && ((joy & J_UP) && !(last_joy & J_UP)))
 
 /* TRUE on first frame that down is pressed on dpad  */
-#define INPUT_DOWN_PRESSED ALLOW_DEFAULT_INPUT_DOWN && ((joy & J_DOWN) && !(last_joy & J_DOWN))
+#define INPUT_DOWN_PRESSED (ALLOW_DEFAULT_INPUT_DOWN && ((joy & J_DOWN) && !(last_joy & J_DOWN)))
 
 /* TRUE on first frame that A button is pressed */
-#define INPUT_A_PRESSED ALLOW_DEFAULT_INPUT_A && ((joy & J_A) && !(last_joy & J_A))
+#define INPUT_A_PRESSED (ALLOW_DEFAULT_INPUT_A && ((joy & J_A) && !(last_joy & J_A)))
 
 /* TRUE on first frame that B button is pressed */
-#define INPUT_B_PRESSED ALLOW_DEFAULT_INPUT_B && ((joy & J_B) && !(last_joy & J_B))
+#define INPUT_B_PRESSED (ALLOW_DEFAULT_INPUT_B && ((joy & J_B) && !(last_joy & J_B)))
 
 /* TRUE on first frame that Start button is pressed */
 #define INPUT_START_PRESSED ((joy & J_START) && !(last_joy & J_START))

src/lib/project/ejectEngineChangelog.ts
@@ -77,6 +77,12 @@ const changes: EngineChange[] = [{
         "src/core/Palette.c",
         "src/core/ScriptRunner_b.c",
     ]
+}, {
+    version: "2.0.0-e9",
+    description: "Fix movement in Adventure mode",
+    modifiedFiles: [
+        "include/Input.h",
+    ]
 }];
 
 const ejectEngineChangelog = (currentVersion: string) => {