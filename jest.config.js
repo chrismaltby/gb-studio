0eb1db1c109169f7a219670f0b88ea838a03e7fc
Chris Maltby <chris.maltby@gmail.com>
2020-10-31 15:17:20 +0000

    Add tests for <VariableSelect />

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
     "\\.(ts|js)x?$": ["./test/custom-transformer"],
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  setupFilesAfterEnv: ["jest-extended"],
+  setupFilesAfterEnv: ["jest-extended", "./test/setup.ts"],
   collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}", "!src/**/*.d.ts"],
   moduleNameMapper: {
     electron: "<rootDir>/test/__mocks__/electronMock.js",
