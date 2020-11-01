7247b63cd4269b8c51d20c0398ad049d24729de3
Chris Maltby <chris.maltby@gmail.com>
2020-11-01 13:23:44 +0000

    Add support for offsetting weapon attack, and flip attack while facing left if using static sprite

--- src/lib/events/eventWeaponAttack.js
+++ src/lib/events/eventWeaponAttack.js
@@ -14,7 +14,17 @@ const fields = [
     type: "actor",
     label: l10n("FIELD_SOURCE"),
     defaultValue: "$self$",
+    width: "50%",
   },  
+  {
+    key: "offset",
+    type: "number",
+    label: l10n("FIELD_OFFSET"),
+    defaultValue: 10,
+    min: 0,
+    max: 64,
+    width: "50%",
+  },    
   {
     key: "collisionGroup",
     label: l10n("FIELD_COLLISION_GROUP"),
@@ -35,8 +45,9 @@ const fields = [
 
 const compile = (input, helpers) => {
   const { weaponAttack, actorSetActive } = helpers;
+  const offset = input.offset === "" || input.offset === undefined || input.offset === null ? 10 : input.offset;
   actorSetActive(input.actorId);
-  weaponAttack(input.spriteSheetId, input.collisionGroup, input.collisionMask);
+  weaponAttack(input.spriteSheetId, offset, input.collisionGroup, input.collisionMask);
 };
 
 module.exports = {
