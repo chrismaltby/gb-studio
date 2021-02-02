7d0dbbd075d65cf567a941012e2b2fc1b38630b1
Richard Ziegler <Richard.invent@gmail.com>
2021-02-02 23:25:29 +1300

    Actor movement speed bug fix by clamping to 4 pixels
    Fixes bug where changing speed mid tile would break collision/movement in topdown 2d

--- appData/src/gb/src/core/ScriptRunner_b.c
+++ appData/src/gb/src/core/ScriptRunner_b.c
@@ -1839,6 +1839,8 @@ void Script_LoadVectors_b() {
  */
 void Script_ActorSetMoveSpeed_b() {
   actors[active_script_ctx.script_actor].move_speed = script_cmd_args[0];
+  actors[active_script_ctx.script_actor].pos.x = actors[active_script_ctx.script_actor].pos.x & 0xFFFC;
+  actors[active_script_ctx.script_actor].pos.y = actors[active_script_ctx.script_actor].pos.y & 0xFFFC;
 }
 
 /*
