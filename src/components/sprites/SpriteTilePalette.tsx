041d6ac216a8e13d64e53486fecdef4fbfffa153
Chris Maltby <chris.maltby@gmail.com>
2021-02-19 20:22:40 +0000

    Improve sprite tile palette scroll

--- src/components/sprites/SpriteTilePalette.tsx
+++ src/components/sprites/SpriteTilePalette.tsx
@@ -167,7 +167,8 @@ const SpriteTilePalette = ({ id }: SpriteTilePaletteProps) => {
         left: 0,
         right: 0,
         bottom: 0,
-        overflow: "auto",
+        overflowX: "auto",
+        overflowY: "scroll",
       }}
     >
       <div
