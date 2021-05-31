719c3430d0e9989208d8d95720328774ee1465d4
Chris Maltby <chris.maltby@gmail.com>
2021-05-31 11:25:19 +0100

    Fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,15 @@
 module.exports = {
+  preset: "ts-jest/presets/js-with-ts",
+  testEnvironment: "jsdom",
   roots: ["<rootDir>/src", "<rootDir>/test"],
-  transform: {
-    "\\.(ts|js)x?$": ["./test/custom-transformer"],
-  },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   setupFilesAfterEnv: ["jest-extended", "./test/setup.ts"],
   collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}", "!src/**/*.d.ts"],
   moduleNameMapper: {
     electron: "<rootDir>/test/__mocks__/electronMock.js",
+    "^store/(.*)$": "<rootDir>/src/store/$1",
+    "^components/(.*)$": "<rootDir>/src/components/$1",
+    "^lib/(.*)$": "<rootDir>/src/lib/$1",
+    "^ui(.*)$": "<rootDir>/src/components/ui/$1",
   },
 };
