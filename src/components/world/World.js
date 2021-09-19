e733f11522f1924f16a4c00828b892a94d7e6053
Tustin2121 <tustin2121@gmail.com>
2021-09-19 01:37:04 -0400

    Make mouse wheel zoom the map
    - Moved the mousewheel event to the World object instead of the window.
    This is to make it so scrolling on the sidebars don't cause the world
    to zoom.
    - On Mac platforms, the ctrl key zoom is still checked for, as the
    panning on touchpads is more user friendly.
    - On Windows/Linux platforms, where mice are more likely to be used, the
    scroll wheel will zoom the map without the ctrl key.

--- src/components/world/World.js
+++ src/components/world/World.js
@@ -33,8 +33,8 @@ class World extends Component {
     window.addEventListener("paste", this.onPaste);
     window.addEventListener("keydown", this.onKeyDown);
     window.addEventListener("mouseup", this.onMouseUp);
-    window.addEventListener("mousewheel", this.onMouseWheel, { passive: false });
     window.addEventListener("resize", this.onWindowResize);
+    this.scrollRef.current.addEventListener("mousewheel", this.onMouseWheel, { passive: false });
 
     const viewContents = this.scrollContentsRef.current;
     // Set zoom ratio on component mount incase it wasn't at 100%
@@ -105,7 +105,7 @@ class World extends Component {
     window.removeEventListener("keydown", this.onKeyDown);
     window.removeEventListener("click", this.onClick);
     window.removeEventListener("mouseup", this.onMouseUp);
-    window.removeEventListener("mousewheel", this.onMouseWheel);
+    this.scrollRef.current.removeEventListener("mousewheel", this.onMouseWheel);
   }
 
   onCopy = e => {
@@ -184,7 +184,9 @@ class World extends Component {
 
   onMouseWheel = e => {
     const { zoomIn, zoomOut } = this.props;
-    if (e.ctrlKey && !this.blockWheelZoom) {
+    // Only check ctrlKey on MacOS, as touch pad is more versitile for scrolling
+    const keyCheck = process.platform === 'darwin'? e.ctrlKey : true;
+    if (keyCheck && !this.blockWheelZoom) {
       e.preventDefault();
       if (e.wheelDelta > 0) {
         zoomIn({section: "world", delta: e.deltaY * 0.5});
