8f7e740f95b40c53f80828143d38d69d8a49ec68
Pau Tomàs <pau.tomas@gmail.com>
2021-12-10 22:45:07 +0000

    Fix error message for no mod songs
    It was using the .uge files one

--- src/components/music/NoSongsMessage.tsx
+++ src/components/music/NoSongsMessage.tsx
@@ -6,7 +6,10 @@ import settingsActions from "store/features/settings/settingsActions";
 import { SettingsState } from "store/features/settings/settingsState";
 import { Button } from "ui/buttons/Button";
 
-export const NoSongsMessage = () => {
+interface NoSongsMessageProps {
+  type: "uge" | "mod";
+}
+export const NoSongsMessage = ({ type }: NoSongsMessageProps) => {
   const dispatch = useDispatch();
   const [showMessage, setShowMessage] = useState(false);
 
@@ -20,22 +23,46 @@ export const NoSongsMessage = () => {
   return (
     <>
       <h2>{l10n("MESSAGE_NO_SONGS_FOUND")}</h2>
-      <p>{l10n("MESSAGE_ADD_UGE_FILES")}</p>
-      <p
-        onClick={() => setShowMessage(!showMessage)}
-        style={{ textDecoration: "underline", cursor: "pointer" }}
-      >
-        {l10n("MESSAGE_WHAT_ABOUT_MOD")}
-      </p>
-      {showMessage ? (
+      {type === "uge" ? (
         <>
-          <p>{l10n("MESSAGE_USE_MOD_FILES")}</p>
-          <Button onClick={() => editSettings({ musicDriver: "gbt" })}>
-            {l10n("MESSAGE_ENABLE_GBT")}
-          </Button>
+          <p>{l10n("MESSAGE_ADD_UGE_FILES")}</p>
+          <p
+            onClick={() => setShowMessage(!showMessage)}
+            style={{ textDecoration: "underline", cursor: "pointer" }}
+          >
+            {l10n("MESSAGE_WHAT_ABOUT_MOD")}
+          </p>
+          {showMessage ? (
+            <>
+              <p>{l10n("MESSAGE_USE_MOD_FILES")}</p>
+              <Button onClick={() => editSettings({ musicDriver: "gbt" })}>
+                {l10n("MESSAGE_ENABLE_GBT")}
+              </Button>
+            </>
+          ) : (
+            ""
+          )}
         </>
       ) : (
-        ""
+        <>
+          <p>{l10n("MESSAGE_ADD_MOD_FILES")}</p>
+          <p
+            onClick={() => setShowMessage(!showMessage)}
+            style={{ textDecoration: "underline", cursor: "pointer" }}
+          >
+            {l10n("MESSAGE_WHAT_ABOUT_UGE")}
+          </p>
+          {showMessage ? (
+            <>
+              <p>{l10n("MESSAGE_USE_UGE_FILES")}</p>
+              <Button onClick={() => editSettings({ musicDriver: "huge" })}>
+                {l10n("MESSAGE_ENABLE_HUGE")}
+              </Button>
+            </>
+          ) : (
+            ""
+          )}
+        </>
       )}
     </>
   );
