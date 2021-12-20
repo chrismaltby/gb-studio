2aafceabc810db46358b8d5c40145d820ed8579d
Atnik <ffchecker@gmx.net>
2021-12-20 15:52:10 +0100

    Updated IfVariableFlagsCompare to work without migration

--- src/lib/events/eventIfVariableFlagsCompare.js
+++ src/lib/events/eventIfVariableFlagsCompare.js
@@ -6,7 +6,7 @@ const groups = ["EVENT_GROUP_CONTROL_FLOW"];
 const autoLabel = (fetchArg) => {
   return l10n("EVENT_IF_FLAGS_COMPARE_LABEL", {
     variable: fetchArg("variable"),
-    flag: String(Number(fetchArg("flag"))),
+    flag: String(Number(fetchArg("flag")) + 1),
   });
 };
 
@@ -20,24 +20,24 @@ const fields = [
     key: "flag",
     type: "select",
     options: [
-      [0x0001, l10n("FIELD_FLAG_1")],
-      [0x0002, l10n("FIELD_FLAG_2")],
-      [0x0004, l10n("FIELD_FLAG_3")],
-      [0x0008, l10n("FIELD_FLAG_4")],
-      [0x0010, l10n("FIELD_FLAG_5")],
-      [0x0020, l10n("FIELD_FLAG_6")],
-      [0x0040, l10n("FIELD_FLAG_7")],
-      [0x0080, l10n("FIELD_FLAG_8")],
-      [0x0100, l10n("FIELD_FLAG_9")],
-      [0x0200, l10n("FIELD_FLAG_10")],
-      [0x0400, l10n("FIELD_FLAG_11")],
-      [0x0800, l10n("FIELD_FLAG_12")],
-      [0x1000, l10n("FIELD_FLAG_13")],
-      [0x2000, l10n("FIELD_FLAG_14")],
-      [0x4000, l10n("FIELD_FLAG_15")],
-      [0x8000, l10n("FIELD_FLAG_16")],
+      [0, l10n("FIELD_FLAG_1")],
+      [1, l10n("FIELD_FLAG_2")],
+      [2, l10n("FIELD_FLAG_3")],
+      [3, l10n("FIELD_FLAG_4")],
+      [4, l10n("FIELD_FLAG_5")],
+      [5, l10n("FIELD_FLAG_6")],
+      [6, l10n("FIELD_FLAG_7")],
+      [7, l10n("FIELD_FLAG_8")],
+      [8, l10n("FIELD_FLAG_9")],
+      [9, l10n("FIELD_FLAG_10")],
+      [10, l10n("FIELD_FLAG_11")],
+      [11, l10n("FIELD_FLAG_12")],
+      [12, l10n("FIELD_FLAG_13")],
+      [13, l10n("FIELD_FLAG_14")],
+      [14, l10n("FIELD_FLAG_15")],
+      [15, l10n("FIELD_FLAG_16")],
     ],
-    defaultValue: 0x0001,
+    defaultValue: 0,
   },
   {
     key: "true",
@@ -77,7 +77,23 @@ const compile = (input, helpers) => {
   const { ifVariableBitwiseValue } = helpers;
   const truePath = input.true;
   const falsePath = input.__disableElse ? [] : input.false;
-  const flags = input.flag;
+  let flags = 0;
+  if (input.flag == 0) flags = 0x0001;
+  if (input.flag == 1) flags = 0x0002;
+  if (input.flag == 2) flags = 0x0004;
+  if (input.flag == 3) flags = 0x0008;
+  if (input.flag == 4) flags = 0x0010;
+  if (input.flag == 5) flags = 0x0020;
+  if (input.flag == 6) flags = 0x0040;
+  if (input.flag == 7) flags = 0x0080;
+  if (input.flag == 8) flags = 0x0100;
+  if (input.flag == 9) flags = 0x0200;
+  if (input.flag == 10) flags = 0x0400;
+  if (input.flag == 11) flags = 0x0800;
+  if (input.flag == 12) flags = 0x1000;
+  if (input.flag == 13) flags = 0x2000;
+  if (input.flag == 14) flags = 0x4000;
+  if (input.flag == 15) flags = 0x8000;
   ifVariableBitwiseValue(input.variable, ".B_AND", flags, truePath, falsePath);
 };
 
