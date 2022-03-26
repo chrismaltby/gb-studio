10df69281b72fdadbc05073d9fe6ae020ff15e81
Julius Liu <juliusl@microsoft.com>
2022-03-25 18:09:49 -0700

    Test: Update unit test

--- test/events/eventIfActorDistanceFromActor.test.js
+++ test/events/eventIfActorDistanceFromActor.test.js
@@ -1,23 +1,59 @@
-import { compile } from "../../src/lib/events/eventIfActorDirection";
+import { compile } from "../../src/lib/events/eventIfActorDistanceFromActor";
 
 test("Should be able to conditionally execute if actor is within range of actor", () => {
   const mockActorSetActive = jest.fn();
   const mockIfActorDistanceFromActor = jest.fn();
+  const mockVariableFromUnion = jest.fn().mockReturnValue(10);
+  const mockTemporaryEntityVariable  = jest.fn().mockReturnValue("ok");
   const truePath = [{ command: "EVENT_END", id: "abc" }];
   const falsePath = [{ command: "EVENT_END", id: "def" }];
   compile(
     {
       actorId: "actor1",
       otherActorId: "actor2",
-      distance: 5,
+      operator: "<=",
+      distance: {
+        type: "number",
+        value: 5
+      },
       true: truePath,
       false: falsePath
     },
     {
       actorSetActive: mockActorSetActive,
-      ifActorDistanceFromActor: mockIfActorDistanceFromActor
+      ifActorDistanceFromActor: mockIfActorDistanceFromActor,
+      variableFromUnion: mockVariableFromUnion,
+      temporaryEntityVariable: mockTemporaryEntityVariable
     }
   );
   expect(mockActorSetActive).toBeCalledWith("actor1");
-  expect(mockIfActorDistanceFromActor).toBeCalledWith(5, "actor2", truePath, falsePath);
+  expect(mockIfActorDistanceFromActor).toBeCalledWith(5, ".LTE", "actor2", truePath, falsePath);
+  expect(mockVariableFromUnion).not.toBeCalled();
+  expect(mockTemporaryEntityVariable).not.toBeCalled();
+
+  let variable = {
+    type: "variable",
+  };
+
+  compile(
+    {
+      actorId: "actor1",
+      otherActorId: "actor2",
+      operator: "==",
+      distance: variable,
+      true: truePath,
+      false: falsePath
+    },
+    {
+      actorSetActive: mockActorSetActive,
+      ifActorDistanceFromActor: mockIfActorDistanceFromActor,
+      variableFromUnion: mockVariableFromUnion,
+      temporaryEntityVariable: mockTemporaryEntityVariable
+    }
+  );
+  
+  expect(mockActorSetActive).toBeCalledWith("actor1");
+  expect(mockIfActorDistanceFromActor).toBeCalledWith(10, ".EQ", "actor2", truePath, falsePath);
+  expect(mockVariableFromUnion).toBeCalledWith(variable, "ok");
+  expect(mockTemporaryEntityVariable).toBeCalledWith(0);
 });
