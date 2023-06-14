65fd7ad7e10852c886ca5c09ea050b0935732760
Pau Tomàs <pau.tomas@gmail.com>
2023-06-15 00:49:06 +0100

    Fix subpatterns in song preview

--- src/components/music/helpers/player.ts
+++ src/components/music/helpers/player.ts
@@ -3,6 +3,7 @@ import storage from "./storage";
 import emulator from "./emulator";
 import { Song } from "lib/helpers/uge/song/Song";
 import { lo, hi } from "lib/helpers/8bit";
+import { SubPatternCell } from "lib/helpers/uge/song/SubPatternCell";
 
 type PlaybackPosition = [number, number];
 
@@ -407,86 +408,75 @@ function patchRom(targetRomFile: Uint8Array, song: Song, startAddr: number) {
     addr += 64 * 2;
   }
 
+  const writeSubPatternCell = (cell: SubPatternCell, isLast: boolean) => {
+    const jump = cell.jump !== null && isLast ? 1 : cell.jump ?? 0;
+
+    buf[addr++] = cell.note ?? 90;
+    buf[addr++] = (jump << 4) | (cell.effectcode ?? 0);
+    buf[addr++] = cell.effectparam ?? 0;
+  };
+
   const subpatternAddr: { [idx: string]: number } = {};
+
   for (let n = 0; n < song.duty_instruments.length; n++) {
     const instr = song.duty_instruments[n];
-
     if (instr.subpattern_enabled) {
       subpatternAddr[`DutySP${instr.index}`] = addr;
-
       const pattern = song.duty_instruments[n].subpattern;
-
       for (let idx = 0; idx < pattern.length; idx++) {
-        const cell = pattern[idx];
-        buf[addr++] = cell.note !== null ? cell.note : 90;
-        buf[addr++] =
-          ((cell.jump !== null ? cell.jump + 1 : 0) << 4) |
-          (cell.effectcode !== null ? cell.effectcode : 0);
-        buf[addr++] = cell.effectparam !== null ? cell.effectparam : 0;
+        writeSubPatternCell(pattern[idx], idx === pattern.length - 1);
       }
     }
   }
 
   for (let n = 0; n < song.wave_instruments.length; n++) {
     const instr = song.wave_instruments[n];
-
     if (instr.subpattern_enabled) {
       subpatternAddr[`WaveSP${instr.index}`] = addr;
-
       const pattern = song.wave_instruments[n].subpattern;
-
       for (let idx = 0; idx < pattern.length; idx++) {
-        const cell = pattern[idx];
-        buf[addr++] = cell.note !== null ? cell.note : 90;
-        buf[addr++] =
-          ((cell.jump !== null ? cell.jump + 1 : 0) << 4) |
-          (cell.effectcode !== null ? cell.effectcode : 0);
-        buf[addr++] = cell.effectparam !== null ? cell.effectparam : 0;
+        writeSubPatternCell(pattern[idx], idx === pattern.length - 1);
       }
     }
   }
 
   for (let n = 0; n < song.noise_instruments.length; n++) {
     const instr = song.noise_instruments[n];
-
     if (instr.subpattern_enabled) {
       subpatternAddr[`NoiseSP${instr.index}`] = addr;
-
       const pattern = song.noise_instruments[n].subpattern;
-
       for (let idx = 0; idx < pattern.length; idx++) {
-        const cell = pattern[idx];
-        buf[addr++] = cell.note !== null ? cell.note : 90;
-        buf[addr++] =
-          ((cell.jump !== null ? cell.jump + 1 : 0) << 4) |
-          (cell.effectcode !== null ? cell.effectcode : 0);
-        buf[addr++] = cell.effectparam !== null ? cell.effectparam : 0;
+        writeSubPatternCell(pattern[idx], idx === pattern.length - 1);
       }
     }
   }
 
+  console.log(subpatternAddr);
+
   for (let n = 0; n < song.duty_instruments.length; n++) {
     const instr = song.duty_instruments[n];
 
-    const nr10 =
+    const sweep =
       (instr.frequency_sweep_time << 4) |
       (instr.frequency_sweep_shift < 0 ? 0x08 : 0x00) |
       Math.abs(instr.frequency_sweep_shift);
-    const nr11 =
+    const lenDuty =
       (instr.duty_cycle << 6) |
       ((instr.length !== null ? 64 - instr.length : 0) & 0x3f);
-    let nr12 =
+    let envelope =
       (instr.initial_volume << 4) |
       (instr.volume_sweep_change > 0 ? 0x08 : 0x00);
     if (instr.volume_sweep_change !== 0)
-      nr12 |= 8 - Math.abs(instr.volume_sweep_change);
-    const nr14 = 0x80 | (instr.length !== null ? 0x40 : 0);
+      envelope |= 8 - Math.abs(instr.volume_sweep_change);
+    const subpattern = subpatternAddr[`DutySP${instr.index}`] ?? 0;
+    const highmask = 0x80 | (instr.length !== null ? 0x40 : 0);
 
-    buf[addr + n * (4 + 2) + 0] = nr10;
-    buf[addr + n * (4 + 2) + 1] = nr11;
-    buf[addr + n * (4 + 2) + 2] = nr12;
-    buf[addr + n * (4 + 2) + 3] = subpatternAddr[`DutySP${instr.index}`] ?? 0;
-    buf[addr + n * (4 + 2) + 5] = nr14;
+    buf[addr + n * (4 + 2) + 0] = sweep;
+    buf[addr + n * (4 + 2) + 1] = lenDuty;
+    buf[addr + n * (4 + 2) + 2] = envelope;
+    buf[addr + n * (4 + 2) + 3] = subpattern & 0xff;
+    buf[addr + n * (4 + 2) + 4] = subpattern >> 8;
+    buf[addr + n * (4 + 2) + 5] = highmask;
   }
   // write the pointer to the duty instruments to the first instruments header index
   writeCurrentAddress();
@@ -496,16 +486,18 @@ function patchRom(targetRomFile: Uint8Array, song: Song, startAddr: number) {
   for (let n = 0; n < song.wave_instruments.length; n++) {
     const instr = song.wave_instruments[n];
 
-    const nr31 = (instr.length !== null ? instr.length : 0) & 0xff;
-    const nr32 = instr.volume << 5;
-    const waveNR = instr.wave_index;
-    const nr34 = 0x80 | (instr.length !== null ? 0x40 : 0);
+    const length = (instr.length !== null ? instr.length : 0) & 0xff;
+    const volume = instr.volume << 5;
+    const waveForm = instr.wave_index;
+    const subpattern = subpatternAddr[`WaveSP${instr.index}`] ?? 0;
+    const highmask = 0x80 | (instr.length !== null ? 0x40 : 0);
 
-    buf[addr + n * (4 + 2) + 0] = nr31;
-    buf[addr + n * (4 + 2) + 1] = nr32;
-    buf[addr + n * (4 + 2) + 2] = waveNR;
-    buf[addr + n * (4 + 2) + 3] = subpatternAddr[`WaveSP${instr.index}`] ?? 0;
-    buf[addr + n * (4 + 2) + 5] = nr34;
+    buf[addr + n * (4 + 2) + 0] = length;
+    buf[addr + n * (4 + 2) + 1] = volume;
+    buf[addr + n * (4 + 2) + 2] = waveForm;
+    buf[addr + n * (4 + 2) + 3] = subpattern & 0xff;
+    buf[addr + n * (4 + 2) + 4] = subpattern >> 8;
+    buf[addr + n * (4 + 2) + 5] = highmask;
   }
   // write the pointer to the wave instruments to the second instruments header index
   writeCurrentAddress();
@@ -515,18 +507,20 @@ function patchRom(targetRomFile: Uint8Array, song: Song, startAddr: number) {
   for (let n = 0; n < song.noise_instruments.length; n++) {
     const instr = song.noise_instruments[n];
 
-    let nr42 =
+    let envelope =
       (instr.initial_volume << 4) |
       (instr.volume_sweep_change > 0 ? 0x08 : 0x00);
     if (instr.volume_sweep_change !== 0)
-      nr42 |= 8 - Math.abs(instr.volume_sweep_change);
-    let param1 = (instr.length !== null ? 64 - instr.length : 0) & 0x3f;
-    if (instr.length !== null) param1 |= 0x40;
-    if (instr.bit_count === 7) param1 |= 0x80;
+      envelope |= 8 - Math.abs(instr.volume_sweep_change);
+    const subpattern = subpatternAddr[`NoiseSP${instr.index}`] ?? 0;
+    let highmask = (instr.length !== null ? 64 - instr.length : 0) & 0x3f;
+    if (instr.length !== null) highmask |= 0x40;
+    if (instr.bit_count === 7) highmask |= 0x80;
 
-    buf[addr + n * (4 + 2) + 0] = nr42;
-    buf[addr + n * (4 + 2) + 1] = subpatternAddr[`NoiseSP${instr.index}`] ?? 0;
-    buf[addr + n * (4 + 2) + 3] = param1;
+    buf[addr + n * (4 + 2) + 0] = envelope;
+    buf[addr + n * (4 + 2) + 1] = subpattern & 0xff;
+    buf[addr + n * (4 + 2) + 2] = subpattern >> 8;
+    buf[addr + n * (4 + 2) + 3] = highmask;
     buf[addr + n * (4 + 2) + 4] = 0;
     buf[addr + n * (4 + 2) + 5] = 0;
   }
