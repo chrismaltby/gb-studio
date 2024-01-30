c0fa466d2059d9b76961e8fe949301b9cb3b4def
Chris Maltby <chris.maltby@gmail.com>
2024-01-30 10:14:11 +0000

    Move global error handling to lib

--- src/app/splash/SplashRoot.js
+++ src/app/splash/SplashRoot.js
@@ -4,34 +4,7 @@ import { AppContainer } from "react-hot-loader";
 import Splash from "components/app/Splash";
 import { initL10N } from "renderer/lib/l10n";
 import { initTheme } from "renderer/lib/theme";
-
-window.addEventListener("error", (error) => {
-  if (error.message.indexOf("dead code elimination") > -1) {
-    return true;
-  }
-  error.stopPropagation();
-  error.preventDefault();
-  document.body.innerHTML = `<div class="GlobalError">
-    <div class="GlobalError__Content">
-      <h2>${error.message}</h2>
-      <p>
-        ${error.filename}L:${error.lineno}C:${error.colno}
-      </p>     
-      <div class="GlobalError__StackTrace">
-        ${
-          error.error &&
-          error.error.stack &&
-          error.error.stack
-            .split("\n")
-            .map((line) => `<div>${line}</div>`)
-            .join("")
-        }
-      </div>
-    </div>       
-    </div>
-  </div>`;
-  return false;
-});
+import "renderer/lib/globalErrorHandling";
 
 const render = () => {
   ReactDOM.render(
