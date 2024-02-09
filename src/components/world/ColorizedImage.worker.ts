110f3fa00335e49f67ed76cf9cd614f65c28895c
Chris Maltby <chris.maltby@gmail.com>
2024-02-09 16:36:58 +0000

    Move color helpers into shared lib

--- src/components/world/ColorizedImage.worker.ts
+++ src/components/world/ColorizedImage.worker.ts
@@ -1,4 +1,4 @@
-import { hex2GBCrgb } from "lib/helpers/color";
+import { hex2GBCrgb } from "shared/lib/helpers/color";
 
 // eslint-disable-next-line no-restricted-globals
 const workerCtx: Worker = self as unknown as Worker;
