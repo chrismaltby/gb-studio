5c1579b520e2b4fc57bb3ea37fbf05dbbf998eed
Nolan Baker <nolan.lee.baker@gmail.com>
2024-10-02 08:24:47 -0500

    obj pal select shows configured palette

--- src/components/forms/ObjPaletteSelect.tsx
+++ src/components/forms/ObjPaletteSelect.tsx
@@ -8,6 +8,7 @@ import {
 } from "ui/form/Select";
 import l10n from "shared/lib/lang/l10n";
 import { SingleValue } from "react-select";
+import { useAppSelector } from "store/hooks";
 
 interface ObjPaletteSelectProps {
   name: string;
@@ -21,25 +22,30 @@ interface ObjPaletteOption {
   colors: string[];
 }
 
-//TODO: change these based on the global palette colors and the selected OBP settings -NB
-const options: ObjPaletteOption[] = [
-  {
-    value: "OBP0",
-    label: "0: OBP0",
-    colors: ["E8F8E0", "B0F088", "", "202850"],
-  },
-  {
-    value: "OBP1",
-    label: "1: OBP1",
-    colors: ["E8F8E0", "509878", "", "202850"],
-  },
-];
-
 export const ObjPaletteSelect: FC<ObjPaletteSelectProps> = ({
   name,
   value = "OBP0",
   onChange,
 }) => {
+  const settings = useAppSelector((state) => state.project.present.settings);
+  const obp0 = settings.defaultOBP0;
+  const obp1 = settings.defaultOBP1;
+  const dmgPal = [settings.customColorsWhite, settings.customColorsLight, settings.customColorsDark, settings.customColorsBlack];
+  const obp0Colors = [dmgPal[obp0[1]], dmgPal[obp0[2]], "", dmgPal[obp0[3]]];
+  const obp1Colors = [dmgPal[obp1[1]], dmgPal[obp1[2]], "", dmgPal[obp1[3]]];
+  const options: ObjPaletteOption[] = [
+    {
+      value: "OBP0",
+      label: "0: OBP0",
+      colors: obp0Colors,
+    },
+    {
+      value: "OBP1",
+      label: "1: OBP1",
+      colors: obp1Colors,
+    },
+  ];
+
   const currentValue = options.find((o) => o.value === value);
   return (
     <Select
