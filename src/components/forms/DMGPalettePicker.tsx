a5b470ada92cb6fb773d293f843f41986c05b144
Nolan Baker <nolan.lee.baker@gmail.com>
2024-10-02 23:27:03 -0500

    better looking... maybe

--- src/components/forms/DMGPalettePicker.tsx
+++ src/components/forms/DMGPalettePicker.tsx
@@ -1,8 +1,8 @@
 import React, { useCallback } from "react";
 import {useAppSelector } from "store/hooks";
-import { FormRow, FormField } from "ui/form/layout/FormLayout";
 import { NumberInput } from "ui/form/NumberInput";
 import { castEventToInt } from "renderer/lib/helpers/castEventValue";
+import styled, { css } from "styled-components";
 
 type DMGPalettePickerProps = {
   name: string
@@ -22,25 +22,28 @@ export const DMGPalettePicker = ({
   const dmgColors = [settings.customColorsWhite, settings.customColorsLight, settings.customColorsDark, settings.customColorsBlack];
   const fields = isSpritePalette ? [1, 2, 3] : [0, 1, 2, 3];
 
+  const DMGPalettePickerStyle = styled.div`
+    display: flex;
+    width: 100%;
+    box-sizing: border-box;`
+
   function getRows() {
     return fields.map((index) =>
-      <FormField name={`${name}_${index}`}>
-        <NumberInput
-          style={{backgroundColor:`#${dmgColors[palette[index]]}`, color:`#${dmgColors[(palette[index]+2)%4]}`}}
-          id={`${name}_${index}`}
-          name={`${name}_${index}`}
-          min={0}
-          max={3}
-          value={palette[index]}
-          onChange={(e) => {
-            let newPalette = [palette[0], palette[1], palette[2], palette[3]];
-            newPalette[index] = castEventToInt(e, 0);
-            onChange(newPalette as [number, number, number, number]);
-          }}
-        />
-      </FormField>
+      <NumberInput
+        style={{backgroundColor:`#${dmgColors[palette[index]]}`, color:`#${dmgColors[(palette[index]+2)%4]}`}}
+        id={`${name}_${index}`}
+        name={`${name}_${index}`}
+        min={0}
+        max={3}
+        value={palette[index]}
+        onChange={(e) => {
+          let newPalette = [palette[0], palette[1], palette[2], palette[3]];
+          newPalette[index] = castEventToInt(e, 0);
+          onChange(newPalette as [number, number, number, number]);
+        }}
+      />
     );
   }
 
-  return (<FormRow>{getRows()}</FormRow>);
+  return <DMGPalettePickerStyle>{getRows()}</DMGPalettePickerStyle>;
 };
