9560f4c48040d0ce19365e8bf42b12f8f6696df2
Chris Maltby <chris.maltby@gmail.com>
2024-10-24 18:27:19 +0100

    When toggling collision layer visibility reset opacity if set below default. Reduces confusion if opacity was set very low

--- src/store/features/settings/settingsMiddleware.ts
+++ src/store/features/settings/settingsMiddleware.ts
@@ -1,21 +1,35 @@
 import { Dispatch, Middleware } from "@reduxjs/toolkit";
 import { RootState } from "store/configureStore";
 import settingsActions from "./settingsActions";
+import { getSettings } from "store/features/settings/settingsState";
+import { defaultProjectSettings } from "consts";
 
 const settingsMiddleware: Middleware<Dispatch, RootState> =
   (store) => (next) => (action) => {
     next(action);
 
-    // When color mode changes reset previewAsMono to false
-    if (
-      settingsActions.editSettings.match(action) &&
-      action.payload.colorMode
-    ) {
-      store.dispatch(
-        settingsActions.editSettings({
-          previewAsMono: false,
-        })
-      );
+    if (settingsActions.editSettings.match(action)) {
+      // When color mode changes reset previewAsMono to false
+      if (action.payload.colorMode) {
+        store.dispatch(
+          settingsActions.editSettings({
+            previewAsMono: false,
+          })
+        );
+      }
+      // When collisions enabled set opacity to a min of 50%
+      if (action.payload.showCollisions) {
+        const state = store.getState();
+        const settings = getSettings(state);
+        const defaultOpacity = defaultProjectSettings.collisionLayerOpacity;
+        if (settings.collisionLayerOpacity < defaultOpacity) {
+          store.dispatch(
+            settingsActions.editSettings({
+              collisionLayerOpacity: defaultOpacity,
+            })
+          );
+        }
+      }
     }
   };
 
