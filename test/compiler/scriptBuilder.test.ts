865c10793d3a7814e4ee1ccabd0309ea1901eb2f
Chris Maltby <chris.maltby@gmail.com>
2024-11-11 13:46:19 +0000

    Add script builder test for codegen of wait codes in dialogue

--- test/compiler/scriptBuilder.test.ts
+++ test/compiler/scriptBuilder.test.ts
@@ -265,6 +265,75 @@ _MY_SCRIPT::
   );
 });
 
+test("Should be able to wait mid-text in dialogue boxes", async () => {
+  const dummyCompiledFont = await getDummyCompiledFont();
+  const output: string[] = [];
+  const scriptEventHandlers = await getTestScriptHandlers();
+  const sb = new ScriptBuilder(output, {
+    scriptEventHandlers,
+    scene: {
+      id: "scene1",
+      name: "Scene 1",
+      symbol: "scene_1",
+      width: 20,
+      height: 18,
+      background: dummyPrecompiledBackground,
+      playerSprite: dummyPrecompiledSpriteSheet,
+      sprites: [],
+      parallax: [],
+      actorsExclusiveLookup: {},
+      type: "TOPDOWN",
+      actors: [],
+      triggers: [],
+      projectiles: [],
+    } as unknown as PrecompiledScene,
+    fonts: [dummyCompiledFont],
+  });
+  sb.textDialogue("Hello!W:5f!World");
+  sb.scriptEnd();
+
+  expect(sb.toScriptString("MY_SCRIPT", false)).toEqual(
+    `.module MY_SCRIPT
+
+.include "vm.i"
+.include "data/game_globals.i"
+
+.globl b_wait_frames, _wait_frames
+
+.area _CODE_255
+
+.LOCAL_TMP0_WAIT_ARGS = -1
+
+___bank_MY_SCRIPT = 255
+.globl ___bank_MY_SCRIPT
+
+_MY_SCRIPT::
+        VM_RESERVE              1
+
+        ; Text Dialogue
+        VM_OVERLAY_CLEAR        0, 0, 20, 4, .UI_COLOR_WHITE, .UI_DRAW_FRAME
+        VM_OVERLAY_MOVE_TO      0, 18, .OVERLAY_SPEED_INSTANT
+        VM_OVERLAY_MOVE_TO      0, 14, .OVERLAY_IN_SPEED
+        VM_OVERLAY_SET_SCROLL   1, 1, 18, 5, .UI_COLOR_WHITE
+        VM_LOAD_TEXT            0
+        .asciz "Hello"
+        VM_DISPLAY_TEXT
+        VM_OVERLAY_WAIT         .UI_MODAL, .UI_WAIT_TEXT
+        VM_SET_CONST            .LOCAL_TMP0_WAIT_ARGS, 5
+        VM_INVOKE               b_wait_frames, _wait_frames, 0, .LOCAL_TMP0_WAIT_ARGS
+        VM_LOAD_TEXT            0
+        .asciz "World"
+        VM_DISPLAY_TEXT_EX      .DISPLAY_PRESERVE_POS, .TEXT_TILE_CONTINUE
+        VM_OVERLAY_WAIT         .UI_MODAL, ^/(.UI_WAIT_WINDOW | .UI_WAIT_TEXT | .UI_WAIT_BTN_A)/
+        VM_OVERLAY_MOVE_TO      0, 18, .OVERLAY_OUT_SPEED
+        VM_OVERLAY_WAIT         .UI_MODAL, ^/(.UI_WAIT_WINDOW | .UI_WAIT_TEXT)/
+
+        ; Stop Script
+        VM_STOP
+`
+  );
+});
+
 test("Should be able to conditionally execute if variable is true with event array paths", async () => {
   const output: string[] = [];
   const scriptEventHandlers = await getTestScriptHandlers();
