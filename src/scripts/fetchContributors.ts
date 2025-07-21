c858b2d457e9b82db08bc2a0b5ac16fb464cc2e9
Chris Maltby <chris.maltby@gmail.com>
2025-07-21 17:48:46 +0100

    Filter out bots when generating contributors.json

--- src/scripts/fetchContributors.ts
+++ src/scripts/fetchContributors.ts
@@ -14,9 +14,8 @@ const ACCESS_TOKEN = process.env.CREDITS_GITHUB_ACCESS_TOKEN;
 const octokit = new Octokit({});
 
 const main = async () => {
-  const contributors = await octokit.paginate(
-    "GET /repos/chrismaltby/gb-studio/contributors",
-    {
+  const contributors = (
+    await octokit.paginate("GET /repos/chrismaltby/gb-studio/contributors", {
       owner: "octocat",
       repo: "gb-studio",
       per_page: 100,
@@ -24,8 +23,11 @@ const main = async () => {
         "X-GitHub-Api-Version": "2022-11-28",
         Authorization: `Bearer ${ACCESS_TOKEN}`,
       },
-    },
-  );
+    })
+  ).filter((contributor) => {
+    // Filter out bots
+    return !contributor.login.includes("[bot]");
+  });
 
   await writeJSON("./contributors.json", contributors, {
     spaces: 2,
