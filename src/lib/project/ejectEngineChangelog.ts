79dbba45e089df1a43a211c2d52ce6cf85c22e1a
Chris Maltby <chris.maltby@gmail.com>
2026-01-13 17:22:57 +0000

    Update gbvm

--- src/lib/project/ejectEngineChangelog.ts
+++ src/lib/project/ejectEngineChangelog.ts
@@ -1063,6 +1063,20 @@ const changes: EngineChange[] = [
       "src/states/adventure.c",
     ],
   },
+  {
+    version: "4.2.0-e27",
+    description: createDescription("Updates", [
+      'Change actor flags from bitfield-bools to "classic" bitmask accessed via SET/CLR/CHK_FLAG macro',
+      "Optimise actors_render performance by moving some checks to earlier in the function",
+    ]),
+    modifiedFiles: [
+      "include/gbs_types.h",
+      "include/vm_actor.h",
+      "src/core/actor.c",
+      "src/core/data_manager.c",
+      "src/core/vm_actor.c",
+    ],
+  },
 ];
 
 export const isKnownEngineVersion = (currentVersion: string): boolean => {
