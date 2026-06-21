915feace4b7486f20e6834170ff899225042e1e7
Chris Maltby <chris.maltby@gmail.com>
2026-06-21 18:05:36 +0100

    entitiesState refactor: extract projectLifecycleReducers

---
+++ src/store/features/entities/reducers/projectLifecycleReducers.ts
@@ -0,0 +1,108 @@
+import { PayloadAction, CaseReducer } from "@reduxjs/toolkit";
+import {
+  EntitiesState,
+  ScriptNormalized,
+} from "shared/lib/entities/entitiesTypes";
+import {
+  ensureSymbolsUnique,
+  updateAllCustomEventsArgs,
+  normalizeEntityResources,
+} from "shared/lib/entities/entitiesHelpers";
+import type { LoadProjectResult } from "lib/project/loadProjectData";
+import { decompressProjectResources } from "shared/lib/resources/compression";
+import {
+  actorsAdapter,
+  triggersAdapter,
+  scenesAdapter,
+  actorPrefabsAdapter,
+  triggerPrefabsAdapter,
+  scriptEventsAdapter,
+  backgroundsAdapter,
+  spriteSheetsAdapter,
+  metaspritesAdapter,
+  metaspriteTilesAdapter,
+  spriteAnimationsAdapter,
+  spriteStatesAdapter,
+  palettesAdapter,
+  customEventsAdapter,
+  musicAdapter,
+  soundsAdapter,
+  fontsAdapter,
+  avatarsAdapter,
+  emotesAdapter,
+  tilesetsAdapter,
+  variablesAdapter,
+  constantsAdapter,
+  notesAdapter,
+  engineFieldValuesAdapter,
+} from "store/features/entities/adapters";
+import {
+  fixAllScenesWithModifiedBackgrounds,
+  updateMonoOverrideIds,
+} from "store/features/entities/reducers/backgroundsReducers";
+
+export const loadProject: CaseReducer<
+  EntitiesState,
+  PayloadAction<LoadProjectResult>
+> = (state, action) => {
+  const uncompressedResources = decompressProjectResources(
+    action.payload.resources,
+  );
+
+  const data = normalizeEntityResources(uncompressedResources);
+
+  actorsAdapter.setAll(state.actors, data.entities.actors || {});
+  triggersAdapter.setAll(state.triggers, data.entities.triggers || {});
+  scenesAdapter.setAll(state.scenes, data.entities.scenes || {});
+  actorPrefabsAdapter.setAll(
+    state.actorPrefabs,
+    data.entities.actorPrefabs || {},
+  );
+  triggerPrefabsAdapter.setAll(
+    state.triggerPrefabs,
+    data.entities.triggerPrefabs || {},
+  );
+  scriptEventsAdapter.setAll(
+    state.scriptEvents,
+    data.entities.scriptEvents || {},
+  );
+  backgroundsAdapter.setAll(state.backgrounds, data.entities.backgrounds || {});
+  spriteSheetsAdapter.setAll(state.spriteSheets, data.entities.sprites || {});
+  metaspritesAdapter.setAll(state.metasprites, data.entities.metasprites || {});
+  metaspriteTilesAdapter.setAll(
+    state.metaspriteTiles,
+    data.entities.metaspriteTiles || {},
+  );
+  spriteAnimationsAdapter.setAll(
+    state.spriteAnimations,
+    data.entities.spriteAnimations || {},
+  );
+  spriteStatesAdapter.setAll(
+    state.spriteStates,
+    data.entities.spriteStates || {},
+  );
+  palettesAdapter.setAll(state.palettes, data.entities.palettes || {});
+  musicAdapter.setAll(state.music, data.entities.music || {});
+  soundsAdapter.setAll(state.sounds, data.entities.sounds || {});
+  fontsAdapter.setAll(state.fonts, data.entities.fonts || {});
+  avatarsAdapter.setAll(state.avatars, data.entities.avatars || {});
+  emotesAdapter.setAll(state.emotes, data.entities.emotes || {});
+  tilesetsAdapter.setAll(state.tilesets, data.entities.tilesets || {});
+  customEventsAdapter.setAll(state.customEvents, data.entities.scripts || {});
+  variablesAdapter.setAll(state.variables, data.entities.variables || {});
+  constantsAdapter.setAll(state.constants, data.entities.constants || {});
+  engineFieldValuesAdapter.setAll(
+    state.engineFieldValues,
+    data.entities.engineFieldValues || {},
+  );
+  notesAdapter.setAll(state.notes, data.entities.notes || {});
+
+  fixAllScenesWithModifiedBackgrounds(state);
+  updateMonoOverrideIds(state);
+  ensureSymbolsUnique(state);
+  updateAllCustomEventsArgs(
+    Object.values(state.customEvents.entities) as ScriptNormalized[],
+    state.scriptEvents.entities,
+    action.payload.scriptEventDefs,
+  );
+};
---
+++ test/store/features/entities/reducers/projectLifecycleReducers.test.ts
@@ -0,0 +1,143 @@
+/* eslint-disable camelcase */
+import reducer, { initialState } from "store/features/entities/entitiesState";
+import { EntitiesState } from "shared/lib/entities/entitiesTypes";
+import projectActions from "store/features/project/projectActions";
+import {
+  dummyCompressedSceneResource,
+  dummyCompressedProjectResources,
+  dummyCompressedBackgroundResource,
+} from "../../../../dummydata";
+import { CompressedProjectResources } from "shared/lib/resources/types";
+
+test("Should fix scene widths if backgrounds has been removed since save", () => {
+  const state: EntitiesState = {
+    ...initialState,
+  };
+
+  const loadData: CompressedProjectResources = {
+    ...dummyCompressedProjectResources,
+    scenes: [
+      {
+        ...dummyCompressedSceneResource,
+        id: "scene1",
+        backgroundId: "missingbg",
+        width: 20,
+        height: 18,
+      },
+    ],
+  };
+
+  const action = projectActions.loadProject.fulfilled(
+    {
+      resources: loadData,
+      path: "project.gbsproj",
+      scriptEventDefs: {},
+      engineSchema: {
+        fields: [],
+        sceneTypes: [],
+        consts: {},
+      },
+      modifiedSpriteIds: [],
+      isMigrated: false,
+    },
+    "randomid",
+    "project.gbsproj",
+  );
+  const newState = reducer(state, action);
+  expect(newState.scenes.entities["scene1"]?.width).toBe(32);
+  expect(newState.scenes.entities["scene1"]?.height).toBe(32);
+});
+
+test("Should fix scene widths if backgrounds have changed dimensions since save", () => {
+  const state: EntitiesState = {
+    ...initialState,
+  };
+
+  const loadData: CompressedProjectResources = {
+    ...dummyCompressedProjectResources,
+    scenes: [
+      {
+        ...dummyCompressedSceneResource,
+        id: "scene1",
+        backgroundId: "bg1",
+        width: 20,
+        height: 18,
+      },
+    ],
+    backgrounds: [
+      {
+        ...dummyCompressedBackgroundResource,
+        id: "bg1",
+        width: 64,
+        height: 40,
+      },
+    ],
+  };
+
+  const action = projectActions.loadProject.fulfilled(
+    {
+      resources: loadData,
+      path: "project.gbsproj",
+      scriptEventDefs: {},
+      engineSchema: {
+        fields: [],
+        sceneTypes: [],
+        consts: {},
+      },
+      modifiedSpriteIds: [],
+      isMigrated: false,
+    },
+    "randomid",
+    "project.gbsproj",
+  );
+  const newState = reducer(state, action);
+  expect(newState.scenes.entities["scene1"]?.width).toBe(64);
+  expect(newState.scenes.entities["scene1"]?.height).toBe(40);
+});
+
+test("Should keep scene widths if backgrounds have NOT changed dimensions since save", () => {
+  const state: EntitiesState = {
+    ...initialState,
+  };
+
+  const loadData: CompressedProjectResources = {
+    ...dummyCompressedProjectResources,
+    scenes: [
+      {
+        ...dummyCompressedSceneResource,
+        id: "scene1",
+        backgroundId: "bg1",
+        width: 20,
+        height: 18,
+      },
+    ],
+    backgrounds: [
+      {
+        ...dummyCompressedBackgroundResource,
+        id: "bg1",
+        width: 20,
+        height: 18,
+      },
+    ],
+  };
+
+  const action = projectActions.loadProject.fulfilled(
+    {
+      resources: loadData,
+      path: "project.gbsproj",
+      scriptEventDefs: {},
+      engineSchema: {
+        fields: [],
+        sceneTypes: [],
+        consts: {},
+      },
+      modifiedSpriteIds: [],
+      isMigrated: false,
+    },
+    "randomid",
+    "project.gbsproj",
+  );
+  const newState = reducer(state, action);
+  expect(newState.scenes.entities["scene1"]?.width).toBe(20);
+  expect(newState.scenes.entities["scene1"]?.height).toBe(18);
+});
