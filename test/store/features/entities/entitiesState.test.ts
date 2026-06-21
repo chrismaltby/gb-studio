faaad881562d5e0296a7dd9c5ea2d1e065abedd4
Chris Maltby <chris.maltby@gmail.com>
2026-06-21 16:03:40 +0100

    entitiesState refactor: extract palettesReducers

--- test/store/features/entities/entitiesState.test.ts
+++ test/store/features/entities/entitiesState.test.ts
@@ -11,14 +11,12 @@ import {
   dummyBackground,
   dummySpriteSheet,
   dummyMusic,
-  dummyPalette,
   dummyCompressedSceneResource,
   dummyCompressedProjectResources,
   dummyCompressedBackgroundResource,
   dummyCustomEventNormalized,
   dummyMusicResource,
 } from "../../../dummydata";
-import { DMG_PALETTE } from "../../../../src/consts";
 import entitiesActions from "../../../../src/store/features/entities/entitiesActions";
 import {
   CompressedBackgroundResourceAsset,
@@ -488,107 +486,6 @@ test("Should be able to delete a variable name by setting blank value", () => {
   const newState = reducer(state, action);
 
   expect(newState.variables.entities["1"]).toBeUndefined();
-});
-
-test("Should be able to add a palette", () => {
-  const state: EntitiesState = {
-    ...initialState,
-  };
-
-  const action = actions.addPalette();
-
-  expect(state.palettes.ids.length).toBe(0);
-  const newState = reducer(state, action);
-  expect(newState.palettes.ids.length).toBe(1);
-  expect(newState.palettes.entities[action.payload.paletteId]?.id).toBe(
-    action.payload.paletteId,
-  );
-  expect(newState.palettes.entities[action.payload.paletteId]?.colors).toEqual(
-    DMG_PALETTE.colors,
-  );
-});
-
-test("Should be able to edit a palette", () => {
-  const state: EntitiesState = {
-    ...initialState,
-    palettes: {
-      entities: {
-        palette1: {
-          ...dummyPalette,
-          id: "palette1",
-        },
-      },
-      ids: ["palette1"],
-    },
-  };
-
-  const action = actions.editPalette({
-    paletteId: "palette1",
-    changes: {
-      colors: ["ff0000", "00ff00", "0000ff", "ffffff"],
-    },
-  });
-
-  const newState = reducer(state, action);
-  expect(newState.palettes.entities[action.payload.paletteId]?.colors).toEqual([
-    "ff0000",
-    "00ff00",
-    "0000ff",
-    "ffffff",
-  ]);
-});
-
-test("Should be able to edit a single palette color without changing others", () => {
-  const state: EntitiesState = {
-    ...initialState,
-    palettes: {
-      entities: {
-        palette1: {
-          ...dummyPalette,
-          id: "palette1",
-          colors: ["050505", "05050D", "050515", "202850"],
-        },
-      },
-      ids: ["palette1"],
-    },
-  };
-
-  const action = actions.editPaletteColor({
-    paletteId: "palette1",
-    colorId: 3,
-    color: "000000",
-  });
-
-  const newState = reducer(state, action);
-  expect(newState.palettes.entities[action.payload.paletteId]?.colors).toEqual([
-    "050505",
-    "05050D",
-    "050515",
-    "000000",
-  ]);
-});
-
-test("Should be able to remove a palette", () => {
-  const state: EntitiesState = {
-    ...initialState,
-    palettes: {
-      entities: {
-        palette1: {
-          ...dummyPalette,
-          id: "palette1",
-        },
-      },
-      ids: ["palette1"],
-    },
-  };
-
-  const action = actions.removePalette({
-    paletteId: "palette1",
-  });
-
-  expect(state.palettes.ids.length).toBe(1);
-  const newState = reducer(state, action);
-  expect(newState.palettes.ids.length).toBe(0);
 });
 
 test("Should be able to add custom event", () => {
