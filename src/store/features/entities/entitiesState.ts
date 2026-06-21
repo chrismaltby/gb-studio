2437ff121dfb7a4c8660b4dea046bc31f1b95adb
Chris Maltby <chris.maltby@gmail.com>
2026-06-21 17:55:30 +0100

    entitiesState refactor: extract assetsReducers

--- src/store/features/entities/entitiesState.ts
+++ src/store/features/entities/entitiesState.ts
@@ -26,16 +26,12 @@ import {
 } from "shared/lib/entities/entitiesTypes";
 import {
   ensureSymbolsUnique,
-  removeAssetEntity,
-  renameAssetEntity,
   paletteName,
   updateAllCustomEventsArgs,
   normalizeEntityResources,
 } from "shared/lib/entities/entitiesHelpers";
 import spriteActions from "store/features/sprite/spriteActions";
 import keyBy from "lodash/keyBy";
-import { Asset, AssetType } from "shared/lib/helpers/assets";
-import { assertUnreachable } from "shared/lib/scriptValue/format";
 import { addNewSongFile } from "store/features/trackerDocument/trackerDocumentState";
 import type { LoadProjectResult } from "lib/project/loadProjectData";
 import { decompressProjectResources } from "shared/lib/resources/compression";
@@ -67,11 +63,6 @@ import {
   notesAdapter,
   engineFieldValuesAdapter,
 } from "store/features/entities/adapters";
-import {
-  localSpriteSheetSelectAll,
-  localBackgroundSelectAll,
-  localMusicSelectAll,
-} from "store/features/entities/helpers";
 import actorsReducers from "store/features/entities/reducers/actorsReducers";
 import triggersReducers from "store/features/entities/reducers/triggersReducers";
 import scenesReducers from "store/features/entities/reducers/scenesReducers";
@@ -99,6 +90,11 @@ import soundsReducers from "store/features/entities/reducers/soundsReducers";
 import emotesReducers from "store/features/entities/reducers/emotesReducers";
 import fontsReducers from "store/features/entities/reducers/fontsReducers";
 import avatarsReducers from "store/features/entities/reducers/avatarsReducers";
+import assetsReducers, {
+  reloadAssets,
+  removedAsset,
+  renamedAsset,
+} from "store/features/entities/reducers/assetsReducers";
 export { selectScriptIds } from "store/features/entities/helpers";
 
 export const initialState: EntitiesState = {
@@ -282,102 +278,6 @@ const loadProject: CaseReducer<
   );
 };
 
-const removedAsset: CaseReducer<
-  EntitiesState,
-  PayloadAction<{
-    assetType: AssetType;
-    asset: Asset;
-  }>
-> = (state, action) => {
-  const { assetType, asset } = action.payload;
-  if (assetType === "backgrounds") {
-    removeAssetEntity(state.backgrounds, backgroundsAdapter, asset);
-    updateMonoOverrideIds(state);
-  } else if (assetType === "tilesets") {
-    removeAssetEntity(state.tilesets, tilesetsAdapter, asset);
-  } else if (assetType === "music") {
-    removeAssetEntity(state.music, musicAdapter, asset);
-  } else if (assetType === "sounds") {
-    removeAssetEntity(state.sounds, soundsAdapter, asset);
-  } else if (assetType === "fonts") {
-    removeAssetEntity(state.fonts, fontsAdapter, asset);
-  } else if (assetType === "avatars") {
-    removeAssetEntity(state.avatars, avatarsAdapter, asset);
-  } else if (assetType === "emotes") {
-    removeAssetEntity(state.emotes, emotesAdapter, asset);
-  } else if (assetType === "sprites") {
-    removeAssetEntity(state.spriteSheets, spriteSheetsAdapter, asset);
-  } else if (assetType === "ui") {
-    // Ignore UI
-  } else {
-    assertUnreachable(assetType);
-  }
-};
-
-const renamedAsset: CaseReducer<
-  EntitiesState,
-  PayloadAction<{
-    assetType: AssetType;
-    asset: Asset;
-    newFilename: string;
-  }>
-> = (state, action) => {
-  const { assetType, asset, newFilename } = action.payload;
-  if (assetType === "backgrounds") {
-    renameAssetEntity(
-      state.backgrounds,
-      backgroundsAdapter,
-      asset,
-      newFilename,
-    );
-    updateMonoOverrideIds(state);
-  } else if (assetType === "tilesets") {
-    renameAssetEntity(state.tilesets, tilesetsAdapter, asset, newFilename);
-  } else if (assetType === "music") {
-    renameAssetEntity(state.music, musicAdapter, asset, newFilename);
-  } else if (assetType === "sounds") {
-    renameAssetEntity(state.sounds, soundsAdapter, asset, newFilename);
-  } else if (assetType === "fonts") {
-    renameAssetEntity(state.fonts, fontsAdapter, asset, newFilename);
-  } else if (assetType === "avatars") {
-    renameAssetEntity(state.avatars, avatarsAdapter, asset, newFilename);
-  } else if (assetType === "emotes") {
-    renameAssetEntity(state.emotes, emotesAdapter, asset, newFilename);
-  } else if (assetType === "sprites") {
-    renameAssetEntity(
-      state.spriteSheets,
-      spriteSheetsAdapter,
-      asset,
-      newFilename,
-    );
-  } else if (assetType === "ui") {
-    // Ignore UI
-  } else {
-    assertUnreachable(assetType);
-  }
-};
-
-/**************************************************************************
- * General Assets
- */
-
-const reloadAssets: CaseReducer<EntitiesState> = (state) => {
-  const now = Date.now();
-
-  const updateTimestamp = <T extends { _v: number }>(obj: T): T => {
-    obj._v = now;
-    return obj;
-  };
-
-  const backgrounds = localBackgroundSelectAll(state).map(updateTimestamp);
-  const spriteSheets = localSpriteSheetSelectAll(state).map(updateTimestamp);
-  const music = localMusicSelectAll(state).map(updateTimestamp);
-
-  backgroundsAdapter.setAll(state.backgrounds, backgrounds);
-  spriteSheetsAdapter.setAll(state.spriteSheets, spriteSheets);
-  musicAdapter.setAll(state.music, music);
-};
-
 // Reducer ---------------------------------------------------------------------
 
 const entitiesSlice = createSlice({
@@ -404,12 +304,7 @@ const entitiesSlice = createSlice({
     ...emotesReducers,
     ...fontsReducers,
     ...avatarsReducers,
-
-    /*
-     * Load assets
-     */
-    removedAsset,
-    renamedAsset,
+    ...assetsReducers,
   },
   extraReducers: (builder) =>
     builder
--- test/store/features/entities/entitiesState.test.ts
+++ test/store/features/entities/entitiesState.test.ts
@@ -11,7 +11,6 @@ import {
   dummyCompressedSceneResource,
   dummyCompressedProjectResources,
   dummyCompressedBackgroundResource,
-  dummyMusic,
 } from "../../../dummydata";
 import entitiesActions from "../../../../src/store/features/entities/entitiesActions";
 import {
@@ -246,34 +245,6 @@ test("Should add new background if loaded while project is open", () => {
   expect(newState.backgrounds.entities["bg1"]?.height).toBe(18);
 });
 
-test("Should remove backgrounds that are deleted while project is open", () => {
-  const state: EntitiesState = {
-    ...initialState,
-    backgrounds: {
-      entities: {
-        bg1: {
-          ...dummyBackground,
-          id: "bg1",
-          filename: "bg1.png",
-        },
-      },
-      ids: ["bg1"],
-    },
-  };
-
-  const action = entitiesActions.removedAsset({
-    assetType: "backgrounds",
-    asset: {
-      filename: "bg1.png",
-      plugin: undefined,
-    },
-  });
-
-  expect(state.backgrounds.ids.length).toBe(1);
-  const newState = reducer(state, action);
-  expect(newState.backgrounds.ids.length).toBe(0);
-});
-
 test("Should add new sprite sheet if loaded while project is open", () => {
   const state: EntitiesState = {
     ...initialState,
@@ -325,60 +296,4 @@ test("Should update sprite sheet if modified while project is open", () => {
   expect(state.spriteSheets.ids.length).toBe(1);
   const newState = reducer(state, action);
   expect(newState.spriteSheets.ids.length).toBe(1);
-});
-
-test("Should remove sprite sheets that are deleted while project is open", () => {
-  const state: EntitiesState = {
-    ...initialState,
-    spriteSheets: {
-      entities: {
-        sprite1: {
-          ...dummySpriteSheet,
-          id: "sprite1",
-          filename: "sprite1.png",
-        },
-      },
-      ids: ["sprite1"],
-    },
-  };
-
-  const action = entitiesActions.removedAsset({
-    assetType: "sprites",
-    asset: {
-      filename: "sprite1.png",
-      plugin: undefined,
-    },
-  });
-
-  expect(state.spriteSheets.ids.length).toBe(1);
-  const newState = reducer(state, action);
-  expect(newState.spriteSheets.ids.length).toBe(0);
-});
-
-test("Should remove music tracks that are deleted while project is open", () => {
-  const state: EntitiesState = {
-    ...initialState,
-    music: {
-      entities: {
-        track1: {
-          ...dummyMusic,
-          id: "track1",
-          filename: "track1.mod",
-        },
-      },
-      ids: ["track1"],
-    },
-  };
-
-  const action = entitiesActions.removedAsset({
-    assetType: "music",
-    asset: {
-      filename: "track1.mod",
-      plugin: undefined,
-    },
-  });
-
-  expect(state.music.ids.length).toBe(1);
-  const newState = reducer(state, action);
-  expect(newState.music.ids.length).toBe(0);
 });
