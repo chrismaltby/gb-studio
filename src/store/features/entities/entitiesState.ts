ee78c997dbbc002cb0b0a1ffd097fe83a540a63f
Chris Maltby <chris.maltby@gmail.com>
2026-06-21 16:24:50 +0100

    entitiesState refactor: extract customEventsReducers

--- src/store/features/entities/entitiesState.ts
+++ src/store/features/entities/entitiesState.ts
@@ -13,10 +13,8 @@ import {
   DRAG_DESTINATION,
   DRAG_TRIGGER,
   DRAG_ACTOR,
-  EVENT_CALL_CUSTOM_EVENT,
   TILE_SIZE,
 } from "consts";
-import { ScriptEventDefs } from "shared/lib/scripts/scriptDefHelpers";
 import { RootState } from "store/storeTypes";
 import settingsActions from "store/features/settings/settingsActions";
 import uuid from "uuid";
@@ -32,20 +30,15 @@ import {
   ScriptEventParentType,
 } from "shared/lib/entities/entitiesTypes";
 import {
-  genEntitySymbol,
   ensureSymbolsUnique,
   removeAssetEntity,
   upsertAssetEntity,
   updateEntitySymbol,
   renameAssetEntity,
-  defaultLocalisedCustomEventName,
   paletteName,
-  updateCustomEventArgs,
   updateAllCustomEventsArgs,
   normalizeEntityResources,
   normalizeSprite,
-  applyReparentFolderToCollection,
-  applyReparentEntityToCollection,
 } from "shared/lib/entities/entitiesHelpers";
 import spriteActions from "store/features/sprite/spriteActions";
 import keyBy from "lodash/keyBy";
@@ -104,8 +97,6 @@ import {
   localSceneSelectAll,
   localSpriteSheetSelectAll,
   localBackgroundSelectAll,
-  localCustomEventSelectTotal,
-  localScriptEventSelectAll,
   localMusicSelectAll,
 } from "store/features/entities/helpers";
 import actorsReducers from "store/features/entities/reducers/actorsReducers";
@@ -119,6 +110,7 @@ import prefabsReducers from "store/features/entities/reducers/prefabsReducers";
 import scriptEventsReducers from "store/features/entities/reducers/scriptEventsReducers";
 import palettesReducers from "store/features/entities/reducers/palettesReducers";
 import variablesReducers from "store/features/entities/reducers/variablesReducers";
+import customEventsReducers from "store/features/entities/reducers/customEventsReducers";
 export { selectScriptIds } from "store/features/entities/helpers";
 
 export const initialState: EntitiesState = {
@@ -901,140 +893,6 @@ const updateMonoOverrideIds = (state: EntitiesState) => {
 };
 
 /**************************************************************************
- * Custom Events
- */
-
-const addCustomEvent: CaseReducer<
-  EntitiesState,
-  PayloadAction<{
-    customEventId: string;
-    defaults?: Partial<ScriptNormalized>;
-  }>
-> = (state, action) => {
-  const customEventsTotal = localCustomEventSelectTotal(state);
-  const newCustomEvent: ScriptNormalized = {
-    id: action.payload.customEventId,
-    name: defaultLocalisedCustomEventName(customEventsTotal),
-    description: "",
-    variables: {},
-    actors: {},
-    ...(action.payload.defaults || {}),
-    symbol: genEntitySymbol(state, `script_${customEventsTotal + 1}`),
-    script: [],
-  };
-  customEventsAdapter.addOne(state.customEvents, newCustomEvent);
-};
-
-const editCustomEvent: CaseReducer<
-  EntitiesState,
-  PayloadAction<{
-    customEventId: string;
-    changes: Partial<ScriptNormalized>;
-  }>
-> = (state, action) => {
-  const patch = { ...action.payload.changes };
-  customEventsAdapter.updateOne(state.customEvents, {
-    id: action.payload.customEventId,
-    changes: patch,
-  });
-};
-
-const setCustomEventSymbol: CaseReducer<
-  EntitiesState,
-  PayloadAction<{ customEventId: string; symbol: string }>
-> = (state, action) => {
-  updateEntitySymbol(
-    state,
-    state.customEvents,
-    customEventsAdapter,
-    action.payload.customEventId,
-    action.payload.symbol,
-  );
-};
-
-const removeCustomEvent: CaseReducer<
-  EntitiesState,
-  PayloadAction<{ customEventId: string; deleteReferences?: boolean }>
-> = (state, action) => {
-  const allScriptEvents = localScriptEventSelectAll(state);
-  const referenceIds: string[] = [];
-
-  for (const scriptEvent of allScriptEvents) {
-    if (
-      scriptEvent.command === EVENT_CALL_CUSTOM_EVENT &&
-      scriptEvent.args?.customEventId === action.payload.customEventId
-    ) {
-      referenceIds.push(scriptEvent.id);
-    }
-  }
-
-  if (action.payload.deleteReferences) {
-    scriptEventsAdapter.removeMany(state.scriptEvents, referenceIds);
-  } else {
-    scriptEventsAdapter.updateMany(
-      state.scriptEvents,
-      referenceIds.map((id) => ({
-        id,
-        changes: {
-          args: { customEventId: undefined },
-        },
-      })),
-    );
-  }
-
-  customEventsAdapter.removeOne(
-    state.customEvents,
-    action.payload.customEventId,
-  );
-};
-
-const refreshCustomEventArgs: CaseReducer<
-  EntitiesState,
-  PayloadAction<{
-    customEventId: string;
-    scriptEventDefs: ScriptEventDefs;
-  }>
-> = (state, action) => {
-  const customEvent = state.customEvents.entities[action.payload.customEventId];
-  if (!customEvent) {
-    return;
-  }
-  updateCustomEventArgs(
-    customEvent,
-    state.scriptEvents.entities,
-    action.payload.scriptEventDefs,
-  );
-};
-
-const reparentCustomEventsFolder: CaseReducer<
-  EntitiesState,
-  PayloadAction<{
-    fromPath: string;
-    toPath: string;
-  }>
-> = (state, action) => {
-  applyReparentFolderToCollection(
-    state.customEvents.entities,
-    action.payload.fromPath,
-    action.payload.toPath,
-  );
-};
-
-const reparentCustomEvent: CaseReducer<
-  EntitiesState,
-  PayloadAction<{
-    customEventId: string;
-    toPath: string;
-  }>
-> = (state, action) => {
-  applyReparentEntityToCollection(
-    state.customEvents.entities,
-    action.payload.customEventId,
-    action.payload.toPath,
-  );
-};
-
-/**************************************************************************
  * Engine Field Values
  */
 
@@ -1099,6 +957,7 @@ const entitiesSlice = createSlice({
     ...scriptEventsReducers,
     ...palettesReducers,
     ...variablesReducers,
+    ...customEventsReducers,
 
     /**************************************************************************
      * Backgrounds
@@ -1107,49 +966,6 @@ const entitiesSlice = createSlice({
     setBackgroundSymbol,
     editBackgroundAutoColor,
     editBackgroundAutoTileFlipOverride,
-
-    /**************************************************************************
-     * Custom Events
-     */
-
-    addCustomEvent: {
-      reducer: addCustomEvent,
-      prepare: (payload?: {
-        customEventId?: string;
-        defaults?: Partial<ScriptNormalized>;
-      }) => {
-        return {
-          payload: {
-            customEventId: payload?.customEventId ?? uuid(),
-            defaults: payload?.defaults,
-          },
-        };
-      },
-    },
-
-    editCustomEvent,
-    setCustomEventSymbol,
-    removeCustomEvent,
-    refreshCustomEventArgs: {
-      reducer: refreshCustomEventArgs,
-      prepare: (payload: {
-        customEventId: string;
-        scriptEventDefs: ScriptEventDefs;
-      }) => {
-        return {
-          payload: {
-            customEventId: payload.customEventId,
-            scriptEventDefs: payload.scriptEventDefs,
-          },
-          meta: {
-            throttle: 1000,
-            key: `refresh_${payload.customEventId}`,
-          },
-        };
-      },
-    },
-    reparentCustomEventsFolder,
-    reparentCustomEvent,
 
     /**************************************************************************
      * Music
