a6f748bedf7386792f80e9ebe3dfd1cd19175f84
Chris Maltby <chris.maltby@gmail.com>
2026-06-21 18:12:37 +0100

    entitiesState refactor: move remaining tests into reducer test files

--- test/store/features/entities/reducers/backgroundsReducers.test.ts
+++ test/store/features/entities/reducers/backgroundsReducers.test.ts
@@ -1,37 +1,13 @@
 /* eslint-disable camelcase */
-import reducer, {
-  initialState,
-} from "../../../../src/store/features/entities/entitiesState";
-import { EntitiesState } from "../../../../src/shared/lib/entities/entitiesTypes";
+import reducer, { initialState } from "store/features/entities/entitiesState";
+import { EntitiesState } from "shared/lib/entities/entitiesTypes";
 import {
   dummySceneNormalized,
   dummyBackground,
-  dummySpriteSheet,
   dummyCompressedBackgroundResource,
-} from "../../../dummydata";
-import entitiesActions from "../../../../src/store/features/entities/entitiesActions";
-import {
-  CompressedBackgroundResourceAsset,
-  SpriteResourceAsset,
-} from "shared/lib/resources/types";
-import { v4 as uuid } from "uuid";
-
-jest.mock("uuid");
-
-const mockUuid = uuid as jest.MockedFunction<typeof uuid>;
-
-beforeEach(() => {
-  let id = 0;
-
-  mockUuid.mockImplementation(() => {
-    id += 1;
-    return `uuid-${id}`;
-  });
-});
-
-afterEach(() => {
-  mockUuid.mockReset();
-});
+} from "../../../../dummydata";
+import entitiesActions from "store/features/entities/entitiesActions";
+import { CompressedBackgroundResourceAsset } from "shared/lib/resources/types";
 
 test("Should fix scene widths if background has changed while project is open", () => {
   const state: EntitiesState = {
@@ -106,57 +82,4 @@ test("Should add new background if loaded while project is open", () => {
   expect(newState.backgrounds.ids.length).toBe(1);
   expect(newState.backgrounds.entities["bg1"]?.width).toBe(20);
   expect(newState.backgrounds.entities["bg1"]?.height).toBe(18);
-});
-
-test("Should add new sprite sheet if loaded while project is open", () => {
-  const state: EntitiesState = {
-    ...initialState,
-  };
-
-  const loadSpriteSheet: SpriteResourceAsset = {
-    ...dummySpriteSheet,
-    id: "sprite1",
-    _resourceType: "sprite",
-    states: [],
-  };
-
-  const action = entitiesActions.loadSprite({
-    data: loadSpriteSheet,
-  });
-
-  expect(state.spriteSheets.ids.length).toBe(0);
-  const newState = reducer(state, action);
-  expect(newState.spriteSheets.ids.length).toBe(1);
-});
-
-test("Should update sprite sheet if modified while project is open", () => {
-  const state: EntitiesState = {
-    ...initialState,
-    spriteSheets: {
-      entities: {
-        sprite1: {
-          ...dummySpriteSheet,
-          id: "sprite1",
-          filename: "sprite1.png",
-        },
-      },
-      ids: ["sprite1"],
-    },
-  };
-
-  const loadSpriteSheet: SpriteResourceAsset = {
-    ...dummySpriteSheet,
-    id: "sprite1",
-    filename: "sprite1.png",
-    _resourceType: "sprite",
-    states: [],
-  };
-
-  const action = entitiesActions.loadSprite({
-    data: loadSpriteSheet,
-  });
-
-  expect(state.spriteSheets.ids.length).toBe(1);
-  const newState = reducer(state, action);
-  expect(newState.spriteSheets.ids.length).toBe(1);
 });
