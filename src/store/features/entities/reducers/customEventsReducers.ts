701fcee9f4e84d9161adc64a673bbea4725abe6b
Chris Maltby <chris.maltby@gmail.com>
2026-06-21 16:24:50 +0100

    entitiesState refactor: extract customEventsReducers

---
+++ src/store/features/entities/reducers/customEventsReducers.ts
@@ -0,0 +1,201 @@
+import {
+  PayloadAction,
+  CaseReducer,
+  SliceCaseReducers,
+} from "@reduxjs/toolkit";
+import { EVENT_CALL_CUSTOM_EVENT } from "consts";
+import { ScriptEventDefs } from "shared/lib/scripts/scriptDefHelpers";
+import uuid from "uuid";
+import {
+  EntitiesState,
+  ScriptNormalized,
+} from "shared/lib/entities/entitiesTypes";
+import {
+  genEntitySymbol,
+  updateEntitySymbol,
+  defaultLocalisedCustomEventName,
+  updateCustomEventArgs,
+  applyReparentFolderToCollection,
+  applyReparentEntityToCollection,
+} from "shared/lib/entities/entitiesHelpers";
+import {
+  scriptEventsAdapter,
+  customEventsAdapter,
+} from "store/features/entities/adapters";
+import {
+  localCustomEventSelectTotal,
+  localScriptEventSelectAll,
+} from "store/features/entities/helpers";
+
+const addCustomEvent: CaseReducer<
+  EntitiesState,
+  PayloadAction<{
+    customEventId: string;
+    defaults?: Partial<ScriptNormalized>;
+  }>
+> = (state, action) => {
+  const customEventsTotal = localCustomEventSelectTotal(state);
+  const newCustomEvent: ScriptNormalized = {
+    id: action.payload.customEventId,
+    name: defaultLocalisedCustomEventName(customEventsTotal),
+    description: "",
+    variables: {},
+    actors: {},
+    ...(action.payload.defaults || {}),
+    symbol: genEntitySymbol(state, `script_${customEventsTotal + 1}`),
+    script: [],
+  };
+  customEventsAdapter.addOne(state.customEvents, newCustomEvent);
+};
+
+const editCustomEvent: CaseReducer<
+  EntitiesState,
+  PayloadAction<{
+    customEventId: string;
+    changes: Partial<ScriptNormalized>;
+  }>
+> = (state, action) => {
+  const patch = { ...action.payload.changes };
+  customEventsAdapter.updateOne(state.customEvents, {
+    id: action.payload.customEventId,
+    changes: patch,
+  });
+};
+
+const setCustomEventSymbol: CaseReducer<
+  EntitiesState,
+  PayloadAction<{ customEventId: string; symbol: string }>
+> = (state, action) => {
+  updateEntitySymbol(
+    state,
+    state.customEvents,
+    customEventsAdapter,
+    action.payload.customEventId,
+    action.payload.symbol,
+  );
+};
+
+const removeCustomEvent: CaseReducer<
+  EntitiesState,
+  PayloadAction<{ customEventId: string; deleteReferences?: boolean }>
+> = (state, action) => {
+  const allScriptEvents = localScriptEventSelectAll(state);
+  const referenceIds: string[] = [];
+
+  for (const scriptEvent of allScriptEvents) {
+    if (
+      scriptEvent.command === EVENT_CALL_CUSTOM_EVENT &&
+      scriptEvent.args?.customEventId === action.payload.customEventId
+    ) {
+      referenceIds.push(scriptEvent.id);
+    }
+  }
+
+  if (action.payload.deleteReferences) {
+    scriptEventsAdapter.removeMany(state.scriptEvents, referenceIds);
+  } else {
+    scriptEventsAdapter.updateMany(
+      state.scriptEvents,
+      referenceIds.map((id) => ({
+        id,
+        changes: {
+          args: { customEventId: undefined },
+        },
+      })),
+    );
+  }
+
+  customEventsAdapter.removeOne(
+    state.customEvents,
+    action.payload.customEventId,
+  );
+};
+
+const refreshCustomEventArgs: CaseReducer<
+  EntitiesState,
+  PayloadAction<{
+    customEventId: string;
+    scriptEventDefs: ScriptEventDefs;
+  }>
+> = (state, action) => {
+  const customEvent = state.customEvents.entities[action.payload.customEventId];
+  if (!customEvent) {
+    return;
+  }
+  updateCustomEventArgs(
+    customEvent,
+    state.scriptEvents.entities,
+    action.payload.scriptEventDefs,
+  );
+};
+
+const reparentCustomEventsFolder: CaseReducer<
+  EntitiesState,
+  PayloadAction<{
+    fromPath: string;
+    toPath: string;
+  }>
+> = (state, action) => {
+  applyReparentFolderToCollection(
+    state.customEvents.entities,
+    action.payload.fromPath,
+    action.payload.toPath,
+  );
+};
+
+const reparentCustomEvent: CaseReducer<
+  EntitiesState,
+  PayloadAction<{
+    customEventId: string;
+    toPath: string;
+  }>
+> = (state, action) => {
+  applyReparentEntityToCollection(
+    state.customEvents.entities,
+    action.payload.customEventId,
+    action.payload.toPath,
+  );
+};
+
+const customEventsReducers = {
+  addCustomEvent: {
+    reducer: addCustomEvent,
+    prepare: (payload?: {
+      customEventId?: string;
+      defaults?: Partial<ScriptNormalized>;
+    }) => {
+      return {
+        payload: {
+          customEventId: payload?.customEventId ?? uuid(),
+          defaults: payload?.defaults,
+        },
+      };
+    },
+  },
+
+  editCustomEvent,
+  setCustomEventSymbol,
+  removeCustomEvent,
+  refreshCustomEventArgs: {
+    reducer: refreshCustomEventArgs,
+    prepare: (payload: {
+      customEventId: string;
+      scriptEventDefs: ScriptEventDefs;
+    }) => {
+      return {
+        payload: {
+          customEventId: payload.customEventId,
+          scriptEventDefs: payload.scriptEventDefs,
+        },
+        meta: {
+          throttle: 1000,
+          key: `refresh_${payload.customEventId}`,
+        },
+      };
+    },
+  },
+  reparentCustomEventsFolder,
+  reparentCustomEvent,
+} satisfies SliceCaseReducers<EntitiesState>;
+
+export default customEventsReducers;
---
+++ test/store/features/entities/reducers/customEventsReducers.test.ts
@@ -0,0 +1,221 @@
+/* eslint-disable camelcase */
+import reducer, { initialState } from "store/features/entities/entitiesState";
+import { EntitiesState } from "shared/lib/entities/entitiesTypes";
+import { dummyCustomEventNormalized } from "../../../../dummydata";
+import entitiesActions from "store/features/entities/entitiesActions";
+
+describe("Custom Events", () => {
+  test("Should be able to add custom event", () => {
+    const state: EntitiesState = {
+      ...initialState,
+    };
+
+    const action = entitiesActions.addCustomEvent();
+
+    expect(state.customEvents.ids.length).toBe(0);
+    const newState = reducer(state, action);
+    expect(newState.customEvents.ids.length).toBe(1);
+    expect(
+      newState.customEvents.entities[action.payload.customEventId]?.name,
+    ).toBe("CUSTOM_EVENT 1");
+    expect(
+      newState.customEvents.entities[action.payload.customEventId]?.script,
+    ).toEqual([]);
+  });
+
+  describe("removeCustomEvent", () => {
+    test("Should remove a custom event and clear references when deleteReferences is false", () => {
+      const state: EntitiesState = {
+        ...initialState,
+        customEvents: {
+          entities: {
+            customEvent1: {
+              ...dummyCustomEventNormalized,
+              id: "customEvent1",
+            },
+          },
+          ids: ["customEvent1"],
+        },
+        scriptEvents: {
+          entities: {
+            scriptEvent1: {
+              id: "scriptEvent1",
+              command: "EVENT_CALL_CUSTOM_EVENT",
+              args: { customEventId: "customEvent1" },
+            },
+            scriptEvent2: {
+              id: "scriptEvent2",
+              command: "EVENT_CALL_CUSTOM_EVENT",
+              args: { customEventId: "customEvent1" },
+            },
+            scriptEvent3: {
+              id: "scriptEvent3",
+              command: "SOME_OTHER_EVENT",
+              args: {},
+            },
+          },
+          ids: ["scriptEvent1", "scriptEvent2", "scriptEvent3"],
+        },
+      };
+
+      const action = entitiesActions.removeCustomEvent({
+        customEventId: "customEvent1",
+        deleteReferences: false,
+      });
+
+      const newState = reducer(state, action);
+
+      expect(newState.customEvents.entities["customEvent1"]).toBeUndefined();
+      expect(newState.scriptEvents.entities["scriptEvent1"]?.id).toEqual(
+        "scriptEvent1",
+      );
+      expect(newState.scriptEvents.entities["scriptEvent2"]?.id).toEqual(
+        "scriptEvent2",
+      );
+      expect(
+        newState.scriptEvents.entities["scriptEvent1"]?.args?.customEventId,
+      ).toBeUndefined();
+      expect(
+        newState.scriptEvents.entities["scriptEvent2"]?.args?.customEventId,
+      ).toBeUndefined();
+      expect(newState.scriptEvents.entities["scriptEvent3"]?.args).toEqual({});
+    });
+
+    test("Should remove a custom event and delete references when deleteReferences is true", () => {
+      const state: EntitiesState = {
+        ...initialState,
+        customEvents: {
+          entities: {
+            customEvent1: {
+              ...dummyCustomEventNormalized,
+              id: "customEvent1",
+            },
+          },
+          ids: ["customEvent1"],
+        },
+        scriptEvents: {
+          entities: {
+            scriptEvent1: {
+              id: "scriptEvent1",
+              command: "EVENT_CALL_CUSTOM_EVENT",
+              args: { customEventId: "customEvent1" },
+            },
+            scriptEvent2: {
+              id: "scriptEvent2",
+              command: "EVENT_CALL_CUSTOM_EVENT",
+              args: { customEventId: "customEvent1" },
+            },
+            scriptEvent3: {
+              id: "scriptEvent3",
+              command: "SOME_OTHER_EVENT",
+              args: {},
+            },
+          },
+          ids: ["scriptEvent1", "scriptEvent2", "scriptEvent3"],
+        },
+      };
+
+      const action = entitiesActions.removeCustomEvent({
+        customEventId: "customEvent1",
+        deleteReferences: true,
+      });
+
+      const newState = reducer(state, action);
+
+      expect(newState.customEvents.entities["customEvent1"]).toBeUndefined();
+      expect(newState.scriptEvents.entities["scriptEvent1"]).toBeUndefined();
+      expect(newState.scriptEvents.entities["scriptEvent2"]).toBeUndefined();
+      expect(newState.scriptEvents.entities["scriptEvent3"]).toBeDefined();
+    });
+
+    test("Should handle case when custom event does not exist", () => {
+      const state: EntitiesState = {
+        ...initialState,
+        customEvents: {
+          entities: {
+            customEvent1: {
+              ...dummyCustomEventNormalized,
+              id: "customEvent1",
+            },
+          },
+          ids: ["customEvent1"],
+        },
+        scriptEvents: {
+          entities: {
+            scriptEvent1: {
+              id: "scriptEvent1",
+              command: "EVENT_CALL_CUSTOM_EVENT",
+              args: { customEventId: "customEvent1" },
+            },
+            scriptEvent2: {
+              id: "scriptEvent2",
+              command: "EVENT_CALL_CUSTOM_EVENT",
+              args: { customEventId: "customEvent1" },
+            },
+          },
+          ids: ["scriptEvent1", "scriptEvent2"],
+        },
+      };
+
+      const action = entitiesActions.removeCustomEvent({
+        customEventId: "nonexistent_custom_event",
+        deleteReferences: false,
+      });
+
+      const newState = reducer(state, action);
+
+      expect(newState.customEvents.entities["customEvent1"]).toBeDefined();
+      expect(
+        newState.scriptEvents.entities["scriptEvent1"]?.args?.customEventId,
+      ).toBe("customEvent1");
+      expect(
+        newState.scriptEvents.entities["scriptEvent2"]?.args?.customEventId,
+      ).toBe("customEvent1");
+    });
+
+    test("Should not modify script events that do not reference the removed custom event", () => {
+      const state: EntitiesState = {
+        ...initialState,
+        customEvents: {
+          entities: {
+            customEvent1: {
+              ...dummyCustomEventNormalized,
+              id: "customEvent1",
+            },
+          },
+          ids: ["customEvent1"],
+        },
+        scriptEvents: {
+          entities: {
+            scriptEvent1: {
+              id: "scriptEvent1",
+              command: "EVENT_CALL_CUSTOM_EVENT",
+              args: { customEventId: "customEvent1" },
+            },
+            scriptEvent2: {
+              id: "scriptEvent2",
+              command: "EVENT_CALL_CUSTOM_EVENT",
+              args: { customEventId: "someOtherCustomEvent" },
+            },
+          },
+          ids: ["scriptEvent1", "scriptEvent2"],
+        },
+      };
+
+      const action = entitiesActions.removeCustomEvent({
+        customEventId: "customEvent1",
+        deleteReferences: false,
+      });
+
+      const newState = reducer(state, action);
+
+      expect(newState.customEvents.entities["customEvent1"]).toBeUndefined();
+      expect(
+        newState.scriptEvents.entities["scriptEvent1"]?.args?.customEventId,
+      ).toBeUndefined();
+      expect(
+        newState.scriptEvents.entities["scriptEvent2"]?.args?.customEventId,
+      ).toBe("someOtherCustomEvent");
+    });
+  });
+});
