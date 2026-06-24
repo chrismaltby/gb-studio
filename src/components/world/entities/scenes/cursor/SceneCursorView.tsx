9303070ec23b1a4c5bd3116f0bcb802ef37267d3
Chris Maltby <chris.maltby@gmail.com>
2026-06-24 15:04:02 +0100

    refactor: make scene cursor view model driven

--- src/components/world/entities/scenes/cursor/SceneCursorView.tsx
+++ src/components/world/entities/scenes/cursor/SceneCursorView.tsx
@@ -1,32 +1,32 @@
 import React from "react";
 import styled, { css } from "styled-components";
-import {
-  TOOL_COLORS,
-  TOOL_COLLISIONS,
-  TOOL_ERASER,
-  BRUSH_16PX,
-  TILE_SIZE,
-  TOOL_ACTORS,
-  TOOL_TRIGGERS,
-} from "consts";
-import { Brush, Tool } from "store/features/editor/editorState";
-import {
-  BrickIcon,
-  CloseIcon,
-  PaintIcon,
-  PlusIcon,
-  ResizeIcon,
-} from "ui/icons/Icons";
+import { TILE_SIZE } from "consts";
+
+export type SceneCursorViewVariant =
+  | "default"
+  | "actors"
+  | "triggers"
+  | "eraser"
+  | "collisions"
+  | "colors";
+
+export interface SceneCursorViewModel {
+  variant: SceneCursorViewVariant;
+  width: number;
+  height: number;
+  bubble?: React.ReactNode;
+}
 
 interface WrapperProps {
-  $tool: Tool;
-  $size: "8px" | "16px";
+  $variant: SceneCursorViewVariant;
+  $width: number;
+  $height: number;
 }
 
 const Wrapper = styled.div<WrapperProps>`
   position: absolute;
-  width: 8px;
-  height: 8px;
+  width: ${(props) => props.$width}px;
+  height: ${(props) => props.$height}px;
   top: 0px;
   left: 0px;
   transform: translate3d(0, 0, 0);
@@ -45,17 +45,8 @@ const Wrapper = styled.div<WrapperProps>`
   }
 
   ${(props) =>
-    props.$size === "16px"
+    props.$variant === "actors"
       ? css`
-          width: 16px;
-          height: 16px;
-        `
-      : ""}
-
-  ${(props) =>
-    props.$tool === TOOL_ACTORS
-      ? css`
-          width: 16px;
           background-color: rgba(247, 45, 220, 0.5);
           outline: 1px solid rgba(140, 0, 177, 0.8);
           pointer-events: all;
@@ -64,7 +55,7 @@ const Wrapper = styled.div<WrapperProps>`
       : ""}
 
   ${(props) =>
-    props.$tool === TOOL_TRIGGERS
+    props.$variant === "triggers"
       ? css`
           background-color: rgba(255, 120, 0, 0.5);
           outline: 1px solid rgba(255, 120, 0, 1);
@@ -74,7 +65,7 @@ const Wrapper = styled.div<WrapperProps>`
       : ""}
 
   ${(props) =>
-    props.$tool === TOOL_ERASER
+    props.$variant === "eraser"
       ? css`
           background-color: rgba(255, 0, 0, 0.8);
           outline: 1px solid rgba(255, 0, 0, 1);
@@ -84,7 +75,7 @@ const Wrapper = styled.div<WrapperProps>`
       : ""}
 
   ${(props) =>
-    props.$tool === TOOL_COLLISIONS
+    props.$variant === "collisions"
       ? css`
           background-color: rgba(250, 40, 40, 0.6);
           outline: 1px solid rgba(250, 40, 40, 0.8);
@@ -93,7 +84,7 @@ const Wrapper = styled.div<WrapperProps>`
       : ""}
 
   ${(props) =>
-    props.$tool === TOOL_COLORS
+    props.$variant === "colors"
       ? css`
           background-color: transparent;
           pointer-events: all;
@@ -126,83 +117,32 @@ const Bubble = styled.div`
 `;
 
 interface SceneCursorViewProps {
-  tool: Tool;
-  brush: Brush;
   x: number;
   y: number;
-  isResizingTrigger: boolean;
+  view: SceneCursorViewModel;
   onMouseMove?: React.MouseEventHandler<HTMLDivElement>;
   onMouseDown?: React.MouseEventHandler<HTMLDivElement>;
 }
 
-const cursorSize = (tool: Tool, brush: Brush): "8px" | "16px" => {
-  return (tool === TOOL_COLORS ||
-    tool === TOOL_COLLISIONS ||
-    tool === TOOL_ERASER) &&
-    brush === BRUSH_16PX
-    ? "16px"
-    : "8px";
-};
-
-const shouldShowBubble = (tool: Tool) => {
-  return (
-    tool === TOOL_ACTORS ||
-    tool === TOOL_TRIGGERS ||
-    tool === TOOL_ERASER ||
-    tool === TOOL_COLORS ||
-    tool === TOOL_COLLISIONS
-  );
-};
-
-const renderBubbleIcon = (tool: Tool, isResizingTrigger: boolean) => {
-  if (tool === TOOL_ACTORS) {
-    return <PlusIcon />;
-  }
-
-  if (tool === TOOL_TRIGGERS) {
-    return isResizingTrigger ? <ResizeIcon /> : <PlusIcon />;
-  }
-
-  if (tool === TOOL_ERASER) {
-    return <CloseIcon />;
-  }
-
-  if (tool === TOOL_COLLISIONS) {
-    return <BrickIcon />;
-  }
-
-  if (tool === TOOL_COLORS) {
-    return <PaintIcon />;
-  }
-
-  return null;
-};
-
 export const SceneCursorView = React.forwardRef<
   HTMLDivElement,
   SceneCursorViewProps
->(
-  (
-    { tool, brush, x, y, isResizingTrigger, onMouseMove, onMouseDown },
-    cursorRef,
-  ) => {
-    return (
-      <Wrapper
-        ref={cursorRef}
-        $tool={tool}
-        $size={cursorSize(tool, brush)}
-        onMouseMove={onMouseMove}
-        onMouseDown={onMouseDown}
-        style={{
-          transform: `translate3d(${x * TILE_SIZE}px, ${y * TILE_SIZE}px, 0)`,
-        }}
-      >
-        {shouldShowBubble(tool) && (
-          <Bubble>{renderBubbleIcon(tool, isResizingTrigger)}</Bubble>
-        )}
-      </Wrapper>
-    );
-  },
-);
+>(({ x, y, view, onMouseMove, onMouseDown }, cursorRef) => {
+  return (
+    <Wrapper
+      ref={cursorRef}
+      $variant={view.variant}
+      $width={view.width}
+      $height={view.height}
+      onMouseMove={onMouseMove}
+      onMouseDown={onMouseDown}
+      style={{
+        transform: `translate3d(${x * TILE_SIZE}px, ${y * TILE_SIZE}px, 0)`,
+      }}
+    >
+      {view.bubble && <Bubble>{view.bubble}</Bubble>}
+    </Wrapper>
+  );
+});
 
 SceneCursorView.displayName = "SceneCursorView";
