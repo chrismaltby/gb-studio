baf8bd80d5822df55a4356cc61c0763d192e0735
Chris Maltby <chris.maltby@gmail.com>
2026-06-26 14:37:56 +0100

    refactor: extract label offset calculation from SceneView

---
+++ src/components/world/entities/scenes/SceneTitle.tsx
@@ -0,0 +1,55 @@
+import React, { useMemo } from "react";
+import styled from "styled-components";
+import { SceneMetadata } from "components/world/entities/scenes/SceneMetadata";
+import { useAppSelectorPick } from "store/hooks";
+import { sceneSelectors } from "store/features/entities/entitiesSelectors";
+import { TILE_SIZE } from "consts";
+import { LabelSpan } from "ui/buttons/LabelButton";
+import { sceneName } from "shared/lib/entities/entitiesHelpers";
+
+interface SceneTitleProps {
+  sceneId: string;
+  sceneIndex: number;
+}
+
+const SceneName = styled.div`
+  white-space: nowrap;
+  font-size: 11px;
+  background-color: ${(props) => props.theme.colors.background};
+  border-radius: 32px;
+  transition: background 0.3s ease-in-out;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+
+export const SceneTitle = ({ sceneId, sceneIndex }: SceneTitleProps) => {
+  const scene = useAppSelectorPick(
+    (state) => sceneSelectors.selectById(state, sceneId),
+    ["notes", "width", "labelColor", "name"],
+  );
+
+  const name = useMemo(
+    () => (scene ? sceneName(scene, sceneIndex) : ""),
+    [sceneIndex, scene],
+  );
+
+  const lastNamePart = useMemo(() => name.replace(/.*[/\\]/, ""), [name]);
+
+  if (!scene) {
+    return null;
+  }
+
+  return (
+    <SceneMetadata sceneId={sceneId}>
+      <SceneName
+        title={scene.notes}
+        style={{
+          maxWidth: scene.width * TILE_SIZE,
+        }}
+      >
+        <LabelSpan color={scene.labelColor}>{lastNamePart}</LabelSpan>
+      </SceneName>
+    </SceneMetadata>
+  );
+};
