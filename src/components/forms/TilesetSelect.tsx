e22930515c8f6ce14d3004a8a29220f3c5bdc6ba
Chris Maltby <chris.maltby@gmail.com>
2026-06-27 14:38:03 +0100

    refactor: optimise tileset select

--- src/components/forms/TilesetSelect.tsx
+++ src/components/forms/TilesetSelect.tsx
@@ -1,5 +1,5 @@
-import React, { FC, useEffect, useState } from "react";
-import { useAppSelector } from "store/hooks";
+import React, { useMemo } from "react";
+import { useAppSelectorPickArray } from "store/hooks";
 import { tilesetSelectors } from "store/features/entities/entitiesSelectors";
 import { UnitType, GridUnitType } from "shared/lib/entities/entitiesTypes";
 import {
@@ -13,10 +13,16 @@ import {
 } from "ui/form/Select";
 import { TileCanvas } from "components/rendering/TileCanvas";
 import uniq from "lodash/uniq";
-import styled from "styled-components";
 import l10n from "shared/lib/lang/l10n";
 import { SingleValue } from "react-select";
-import { TilesetAsset } from "shared/lib/resources/types";
+
+type TilesetSummary = {
+  id: string;
+  name: string;
+  plugin?: string;
+  imageWidth: number;
+  imageHeight: number;
+};
 
 interface TilesetSelectProps extends SelectCommonProps {
   name: string;
@@ -33,34 +39,29 @@ interface TilesetSelectProps extends SelectCommonProps {
 }
 
 interface TilesetOption extends Option {
-  tileset?: TilesetAsset;
+  tileset?: TilesetSummary;
 }
 
 interface TilesetOptGroup extends OptGroup {
   options: TilesetOption[];
 }
 
-const Wrapper = styled.div`
-  position: relative;
-`;
-
 const buildOptions = (
-  memo: OptGroup[],
+  memo: TilesetOptGroup[],
   plugin: string | undefined,
-  tilesets: TilesetAsset[],
+  tilesets: TilesetSummary[],
 ) => {
   memo.push({
     label: plugin ? plugin : "",
-    options: tilesets.map((tileset) => {
-      return {
-        value: tileset.id,
-        label: tileset.name,
-      };
-    }),
+    options: tilesets.map((tileset) => ({
+      value: tileset.id,
+      label: tileset.name,
+      tileset,
+    })),
   });
 };
 
-export const TilesetSelect: FC<TilesetSelectProps> = ({
+const TilesetSelectComponent = ({
   value,
   tileIndex,
   onChange,
@@ -69,26 +70,37 @@ export const TilesetSelect: FC<TilesetSelectProps> = ({
   optionalLabel,
   filters,
   ...selectProps
-}) => {
-  const tilesets = useAppSelector((state) => tilesetSelectors.selectAll(state));
-  const [options, setOptions] = useState<TilesetOptGroup[]>([]);
-  const [currentTileset, setCurrentTileset] = useState<TilesetAsset>();
-  const [currentValue, setCurrentValue] = useState<TilesetOption>();
+}: TilesetSelectProps) => {
+  const tilesets = useAppSelectorPickArray(tilesetSelectors.selectAll, [
+    "id",
+    "name",
+    "plugin",
+    "imageWidth",
+    "imageHeight",
+  ] as const);
 
-  useEffect(() => {
-    const filteredTilesets = filters
-      ? tilesets.filter((tileset) => {
-          if (filters.width && tileset.imageWidth !== filters.width) {
-            return false;
-          }
-          if (filters.height && tileset.imageHeight !== filters.height) {
-            return false;
-          }
-          return true;
-        })
-      : tilesets;
+  const filterWidth = filters?.width;
+  const filterHeight = filters?.height;
+
+  const filteredTilesets = useMemo(() => {
+    if (filterWidth === undefined && filterHeight === undefined) {
+      return tilesets;
+    }
+    return tilesets.filter((tileset) => {
+      if (filterWidth !== undefined && tileset.imageWidth !== filterWidth) {
+        return false;
+      }
+      if (filterHeight !== undefined && tileset.imageHeight !== filterHeight) {
+        return false;
+      }
+      return true;
+    });
+  }, [tilesets, filterWidth, filterHeight]);
+
+  const options = useMemo(() => {
     const plugins = uniq(filteredTilesets.map((s) => s.plugin || "")).sort();
-    const options = plugins.reduce(
+
+    return plugins.reduce<TilesetOptGroup[]>(
       (memo, plugin) => {
         buildOptions(
           memo,
@@ -100,37 +112,43 @@ export const TilesetSelect: FC<TilesetSelectProps> = ({
         return memo;
       },
       optional
-        ? ([
+        ? [
             {
               label: "",
               options: [
-                { value: "", label: optionalLabel || l10n("FIELD_NONE") },
+                {
+                  value: "",
+                  label: optionalLabel || l10n("FIELD_NONE"),
+                },
               ],
             },
-          ] as TilesetOptGroup[])
-        : ([] as TilesetOptGroup[]),
+          ]
+        : [],
     );
+  }, [filteredTilesets, optional, optionalLabel]);
 
-    setOptions(options);
-  }, [tilesets, optional, optionalLabel, filters]);
+  const currentTileset = useMemo(
+    () => tilesets.find((tileset) => tileset.id === value),
+    [tilesets, value],
+  );
 
-  useEffect(() => {
-    setCurrentTileset(tilesets.find((v) => v.id === value));
-  }, [tilesets, value]);
-
-  useEffect(() => {
+  const currentValue = useMemo<TilesetOption | undefined>(() => {
     if (currentTileset) {
-      setCurrentValue({
+      return {
         value: currentTileset.id,
-        label: `${currentTileset.name}`,
+        label: currentTileset.name,
         tileset: currentTileset,
-      });
-    } else if (optional) {
-      setCurrentValue({
-        value: "",
-        label: optionalLabel || "None",
-      });
+      };
     }
+
+    if (optional) {
+      return {
+        value: "",
+        label: optionalLabel || l10n("FIELD_NONE"),
+      };
+    }
+
+    return undefined;
   }, [currentTileset, optional, optionalLabel]);
 
   const onSelectChange = (newValue: SingleValue<Option>) => {
@@ -140,47 +158,47 @@ export const TilesetSelect: FC<TilesetSelectProps> = ({
   };
 
   return (
-    <Wrapper>
-      <Select
-        value={currentValue}
-        options={options}
-        onChange={onSelectChange}
-        formatOptionLabel={(option: TilesetOption) => {
-          return (
-            <OptionLabelWithPreview
-              preview={
-                option.value ? (
-                  <TileCanvas
-                    tilesetId={option.value}
-                    tileIndex={tileIndex}
-                    tileSize={units as GridUnitType}
-                  />
-                ) : null
-              }
-            >
-              <FormatFolderLabel label={option.label} />
-            </OptionLabelWithPreview>
-          );
-        }}
-        components={{
-          SingleValue: () => (
-            <SingleValueWithPreview
-              preview={
-                value ? (
-                  <TileCanvas
-                    tilesetId={value}
-                    tileIndex={tileIndex}
-                    tileSize={units as GridUnitType}
-                  />
-                ) : null
-              }
-            >
-              <FormatFolderLabel label={currentValue?.label} />
-            </SingleValueWithPreview>
-          ),
-        }}
-        {...selectProps}
-      />
-    </Wrapper>
+    <Select
+      value={currentValue}
+      options={options}
+      onChange={onSelectChange}
+      formatOptionLabel={(option: TilesetOption) => {
+        return (
+          <OptionLabelWithPreview
+            preview={
+              option.value ? (
+                <TileCanvas
+                  tilesetId={option.value}
+                  tileIndex={tileIndex}
+                  tileSize={units as GridUnitType}
+                />
+              ) : null
+            }
+          >
+            <FormatFolderLabel label={option.label} />
+          </OptionLabelWithPreview>
+        );
+      }}
+      components={{
+        SingleValue: () => (
+          <SingleValueWithPreview
+            preview={
+              value ? (
+                <TileCanvas
+                  tilesetId={value}
+                  tileIndex={tileIndex}
+                  tileSize={units as GridUnitType}
+                />
+              ) : null
+            }
+          >
+            <FormatFolderLabel label={currentValue?.label} />
+          </SingleValueWithPreview>
+        ),
+      }}
+      {...selectProps}
+    />
   );
 };
+
+export const TilesetSelect = React.memo(TilesetSelectComponent);
